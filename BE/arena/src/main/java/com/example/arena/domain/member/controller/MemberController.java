52f1c8df436ae949b780767ba68acf439eaa67ac
ssafy11thseoul <ssaf010203@gmail.com>
2024-07-09 16:57:23 +0900

    init : init setting
    Former-commit-id: 733a8a598c4aabcbc85c96a6e5fded45b3e5538f

--- BE/arena/src/main/java/com/example/arena/domain/member/controller/MemberController.java
+++ BE/arena/src/main/java/com/example/arena/domain/member/controller/MemberController.java
@@ -1,3 +1,9 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:25052a8a897c5da19f2fb69ddd8cb6b5cff40409f2afd62de0832a1640f70b97
-size 257
+package com.example.arena.domain.member.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/member")
+public class MemberController {
+}
