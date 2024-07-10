855fbac8c41ca52be1c050e00a9856140fbe3723
진주원 <drasgon@naver.com>
2024-07-10 14:06:08 +0900

    S11P11A807-29 Group Logic init Setting
    Former-commit-id: 3fd4333c032169534b5172fe812c63e05f416222

---
+++ BE/arena/src/main/java/com/example/arena/domain/group/entity/Group.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:268c01057479d62466a032ccab71b98b9e00585e2eab4ae80baa09fe97abc506
+size 445
---
+++ BE/arena/src/main/java/com/example/arena/domain/group/repository/GroupRepository.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:61e5fbc4807930589ca8712f3389a5447256f583303fc009c0d85d42b93942f1
+size 264
