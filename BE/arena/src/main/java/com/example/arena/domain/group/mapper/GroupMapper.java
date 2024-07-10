42e39dc501e1d0342e3be5810888e3976e827719
진주원 <drasgon@naver.com>
2024-07-10 14:35:58 +0900

    S11P11A807-29 Group Mapper Setting
    Former-commit-id: a4eea90d6c4908a1c0dc3d21e1c2e77bba73e3fd

--- BE/arena/src/main/java/com/example/arena/domain/group/mapper/GroupMapper.java
+++ BE/arena/src/main/java/com/example/arena/domain/group/mapper/GroupMapper.java
@@ -1,7 +1,3 @@
-package com.example.arena.domain.group.mapper;
-
-import org.springframework.stereotype.Component;
-
-@Component
-public class GroupMapper {
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:6eebef4f1ec81bc063bc7c74e9f88c45bf433d81453d0a6a4b8247e5d3918417
+size 745
