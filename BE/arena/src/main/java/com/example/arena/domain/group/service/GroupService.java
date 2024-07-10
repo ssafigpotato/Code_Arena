f5e229d840a03e69a26a8ba4375703c48a533367
진주원 <drasgon@naver.com>
2024-07-10 14:22:48 +0900

    S11P11A807-29 Group Controller
    Former-commit-id: 06a6e9acf8e2531d6f127386583df58f985a7ed2

--- BE/arena/src/main/java/com/example/arena/domain/group/service/GroupService.java
+++ BE/arena/src/main/java/com/example/arena/domain/group/service/GroupService.java
@@ -1,14 +1,3 @@
-package com.example.arena.domain.group.service;
-
-import com.example.arena.domain.group.entity.Group;
-import com.example.arena.domain.group.repository.GroupRepository;
-import org.springframework.stereotype.Service;
-
-@Service
-public class GroupService {
-    private final GroupRepository groupRepository;
-
-    public GroupService(GroupRepository groupRepository) {
-        this.groupRepository = groupRepository;
-    }
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:a6268c4c0b4f4b1c509b37ca83ae53e6e708734200be3e3e729a17fa6a5b3e47
+size 731
