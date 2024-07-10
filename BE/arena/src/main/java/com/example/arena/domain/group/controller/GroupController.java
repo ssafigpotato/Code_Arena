a1835fb1631c23fbbf720f78f76660fe88b7bfb4
진주원 <drasgon@naver.com>
2024-07-10 14:35:49 +0900

    S11P11A807-29 Group Controller 구현 (팀원 초대 및 수락 로직 구현 필요)
    Former-commit-id: ed6aabe95b49e18acf46cfd4625de2c880c7b884

--- BE/arena/src/main/java/com/example/arena/domain/group/controller/GroupController.java
+++ BE/arena/src/main/java/com/example/arena/domain/group/controller/GroupController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a901f72a36575798c77be2f55a9d059eb47e2ceef97bc01abe1abfdfb3b52926
-size 1306
+oid sha256:78b2fdb08c850860c8871f58e10774143b9749a0e736feb0538038eb5e1b565d
+size 1555
