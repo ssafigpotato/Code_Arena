b29298d7f844b0798c6549860b3935cbf4e8d228
Dohyungh <bley1217@gmail.com>
2024-07-10 17:38:10 +0900

    S11P11A807-12 docs: 회원가입페이지 figma 완성
    Former-commit-id: dc9463909f20aa5c17f9834dcbf71a987ec33862

--- BE/arena/src/main/java/com/example/arena/domain/member/dto/response/MemberResponse.java
+++ BE/arena/src/main/java/com/example/arena/domain/member/dto/response/MemberResponse.java
@@ -1,3 +1,4 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:909b612a6aaa2ace2c835faf2593c44c07930a4c9351da20ad3cbe79c1dbfb4b
-size 87
+package com.example.arena.domain.member.dto.response;
+
+public class MemberResponse {
+}
--- BE/arena/src/main/java/com/example/arena/domain/member/repository/MemberRepository.java
+++ BE/arena/src/main/java/com/example/arena/domain/member/repository/MemberRepository.java
@@ -1,3 +1,5 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:1befce3855cfdf5cac185bd26128eb292a5030893a96e63050167077824091c8
-size 92
+package com.example.arena.domain.member.repository;
+
+
+public interface MemberRepository {
+}
