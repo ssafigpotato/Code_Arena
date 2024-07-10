2cf1011a16685bbef969bc17726b112b51ea7e08
진주원 <drasgon@naver.com>
2024-07-10 10:38:41 +0900

    init : Directory Structure Example
    Former-commit-id: fe52f495fab2c68684e53a49e62a49ee0a5b9b0e

--- BE/arena/src/main/java/com/example/arena/domain/member/dto/response/MemberResponse.java
+++ BE/arena/src/main/java/com/example/arena/domain/member/dto/response/MemberResponse.java
@@ -1,4 +1,3 @@
-package com.example.arena.domain.member.dto.response;
-
-public class MemberResponse {
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:909b612a6aaa2ace2c835faf2593c44c07930a4c9351da20ad3cbe79c1dbfb4b
+size 87
