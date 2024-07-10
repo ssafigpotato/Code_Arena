4f532e664d7e5e80798de22603d6ebdfce5f4adc
진주원 <drasgon@naver.com>
2024-07-10 14:06:08 +0900

    S11P11A807-29 Group Logic init Setting
    Former-commit-id: a665785b5871e1ad14bf8889787226e56202b09e

---
+++ BE/arena/src/main/java/com/example/arena/domain/group/controller/GroupController.java
@@ -0,0 +1,15 @@
+package com.example.arena.domain.group.controller;
+
+import com.example.arena.domain.group.service.GroupService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/group")
+public class GroupController {
+    private final GroupService groupService;
+
+    public GroupController(GroupService groupService) {
+        this.groupService = groupService;
+    }
+}
