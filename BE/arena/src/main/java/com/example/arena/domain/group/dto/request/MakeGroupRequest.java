3122e1f1b1f722a7407717d95b7f7f60bbd378d8
진주원 <drasgon@naver.com>
2024-07-10 14:45:02 +0900

    S11P11A807-29 Group Type (PUBLIC , PRIVATE) 추가
    Former-commit-id: a6c9ec65025ae20f1c58716c1c19252291d7e525

--- BE/arena/src/main/java/com/example/arena/domain/group/dto/request/MakeGroupRequest.java
+++ BE/arena/src/main/java/com/example/arena/domain/group/dto/request/MakeGroupRequest.java
@@ -1,5 +1,6 @@
 package com.example.arena.domain.group.dto.request;
 
+import com.example.arena.domain.group.entity.GroupType;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -9,4 +10,5 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class MakeGroupRequest {
     private String groupName;
+    private GroupType groupType;
 }
