19edae61da3566b0f4d516899a6d07ac9b5594af
inception010203 <inception010203@gmail.com>
2024-07-15 16:14:41 +0900

    S11P12A807-100 feat: landing-page wrapper 스타일 정의
    Former-commit-id: 8cca64f8abb8e30252965c91f5b51873fb33e493

--- FE/code-arena/src/app/page.tsx
+++ FE/code-arena/src/app/page.tsx
@@ -1,3 +1,3 @@
-export default function Index() {
-  return <main></main>;
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:92ec514f9813fabd34a36a0137c5d5d56f43b3d6671494440565c80f1953086e
+size 560
