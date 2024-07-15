01545c72e07fd7c789e4cf3bc264b72e03a6d8d1
Dohyungh <bley1217@gmail.com>
2024-07-15 17:54:49 +0900

    S11P12A807-40 feat: login page 작성 시작
    Former-commit-id: bb31f27aac41d2672807012fc11d21c36c10d3cd

--- FE/code-arena/src/app/login/page.tsx
+++ FE/code-arena/src/app/login/page.tsx
@@ -1,7 +1,3 @@
-export default function write() {
-  return (
-    <>
-      <h1></h1>
-    </>
-  );
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:a86a8b885a409f2fb07fd1460d588132dc6f94796ab3c3f0728bedb2bf3908c2
+size 3135
