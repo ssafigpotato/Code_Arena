6dd15ca298f78e3e0655d6e906c0206acfe059be
진주원 <drasgon@naver.com>
2024-07-15 17:03:32 +0900

    [FEAT : BE S11P12A807-70] Group Controller 수정
    Former-commit-id: 56eb0dcfdcf62674ad2c25fb2da80dfe6ee0f77a

--- BE/arena/src/main/java/com/example/arena/domain/group/controller/GroupController.java
+++ BE/arena/src/main/java/com/example/arena/domain/group/controller/GroupController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:87f66e47bd50449e6603560e29d7b1ef6b3fb4dd356f238e43b6843ff41b9a05
-size 2506
+oid sha256:fdb1b50c91fce6df7278467ecc2e258318efd272010e29d2b35e51ba3be3c2ed
+size 2490
