24876e216c14ae566c236b82de3b09dcb5affee5
ryucream612 <ryucream612@naver.com>
2024-07-15 17:56:12 +0900

    S11P12A807-32 feat: dashboard의 RoomList 초안 작업 - done: 구글 매터리얼 아이콘 CDN 임포트
    Former-commit-id: 4f7fe567470adb400e3eb59a93b6911e0a6f7f2f

--- FE/code-arena/src/components/dashboard/RoomList.tsx
+++ FE/code-arena/src/components/dashboard/RoomList.tsx
@@ -1,14 +1,3 @@
-import styled from "styled-components";
-
-const RoomList: () => JSX.Element = () => {
-  return <RoomTitle>방 목록</RoomTitle>;
-};
-
-const RoomTitle = styled.div`
-  font-size: 24px;
-  font-weight: 700;
-  padding: 20px 0px;
-  background-color: ;
-`;
-
-export default RoomList;
+version https://git-lfs.github.com/spec/v1
+oid sha256:97c597cba1d73963d128ee39b65e481f3f009410d886c750f6d208555565257b
+size 6926
