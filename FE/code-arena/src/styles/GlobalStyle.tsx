0330d840c7beaecf1b831c6fdb2b433f3e01702f
inception010203 <inception010203@gmail.com>
2024-07-15 15:13:25 +0900

    feat: 사용자정의 변수 설정
    Former-commit-id: 0580917c57b5d1ef5ac363923b51f18789286a31

--- FE/code-arena/src/styles/GlobalStyle.tsx
+++ FE/code-arena/src/styles/GlobalStyle.tsx
@@ -5,6 +5,15 @@ import "pretendard/dist/web/static/pretendard.css";
 
 const GlobalStyle = createGlobalStyle`
   ${reset}
+   :root {
+    --bg-color: #27374D;
+    --primary-color: #526D82;
+    --secondary-color: #9DB2Bf;
+    --light-color: #DDE6ED;
+    --yellow-color: #FFD91C;
+    --red-color: #F38080;
+  }
+
   body {
     font-family: 'Pretendard', sans-serif;
   }
