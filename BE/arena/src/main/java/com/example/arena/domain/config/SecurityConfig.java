cc5ba2b584c8ab24c4320f2ca76bf99dec406009
이정혁 <gacomoria14@naver.com>
2024-07-15 12:48:59 +0900

    S11P11A807-28 fix: SecurityConfig 수정
    Former-commit-id: 40cd7a1e4a97746fd9581a91a0e016e4776cab7c

--- BE/arena/src/main/java/com/example/arena/domain/config/SecurityConfig.java
+++ BE/arena/src/main/java/com/example/arena/domain/config/SecurityConfig.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a333cc74fc4e6853ddd2cdcdc95eb85b9f1d5da57bf593a2cc28e14dc3e48884
-size 3876
+oid sha256:ca2f07a8d740df7b6425b12a6626846cd91d04f616aece753eb5d92058260a84
+size 3911
