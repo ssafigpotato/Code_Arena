f8b76c6797d4e3da4f491066b16c962528239df2
inception010203 <inception010203@gmail.com>
2024-07-15 17:44:40 +0900

    S11P12A807-100 feat: 랜딩페이지 초기설정 및 헤더 푸터 초기설정
    Former-commit-id: a10c3126fe4330b6c03de8f4da48cd0bf4845d6f

--- FE/code-arena/src/app/layout.tsx
+++ FE/code-arena/src/app/layout.tsx
@@ -1,19 +1,3 @@
-import StyledComponentsRegistry from "../lib/registry";
-import GlobalStyle from "../styles/GlobalStyle";
-
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return (
-    <html>
-      <body>
-        <StyledComponentsRegistry>
-          <GlobalStyle />
-          {children}
-        </StyledComponentsRegistry>
-      </body>
-    </html>
-  );
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:1feb1d919db416733c20606bf070c0b456112abf730971e8498f292a4af461cf
+size 548
