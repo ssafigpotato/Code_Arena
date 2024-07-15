863c035d2650a00fadc78b267f47936294d704e1
inception010203 <inception010203@gmail.com>
2024-07-15 17:44:40 +0900

    S11P12A807-100 feat: 랜딩페이지 초기설정 및 헤더 푸터 초기설정
    Former-commit-id: 1a56ec69afe568705b10a9cf03583dec3db3956f

--- FE/code-arena/src/app/page.tsx
+++ FE/code-arena/src/app/page.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:92ec514f9813fabd34a36a0137c5d5d56f43b3d6671494440565c80f1953086e
-size 560
+oid sha256:c3acccf03a43478296f585f1d9b29b728f7f3a7edb1cb47e4118f53af7927733
+size 1479
---
+++ FE/code-arena/src/components/common/footer/footer.tsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:69a10d19884eacae2e2363024965585ee1726577c149ebd6429953d2b7bc39e2
+size 412
