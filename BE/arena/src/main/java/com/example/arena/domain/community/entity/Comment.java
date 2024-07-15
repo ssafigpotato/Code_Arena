1fd2e224c089ab0f5ab5c241634e71774e8fbac4
허동민 <gjehdals456@gmail.com>
2024-07-15 17:42:55 +0900

    S11P12A807-107 refactor : 댓글 기능 리팩토링 - need : swagger 테스트 후 로직 수정 필요 - need : 좋아요 기능 리팩토링 필요 - need : 대댓글 기능 추가 필요
    Former-commit-id: 98e2d512e87513dde3b3aa1af6fbfc27b12f14e4

--- BE/arena/src/main/java/com/example/arena/domain/community/entity/Comment.java
+++ BE/arena/src/main/java/com/example/arena/domain/community/entity/Comment.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cf20a5b105d4b84d09d805c01ad7597ee13c8fcdfbadb0c7a0e8701bd06ff0e4
-size 836
+oid sha256:5be25cdd5c4c6e1727a0c77d8c3a634c54cfff93b32a0be5fa7644efc9de9be9
+size 947
