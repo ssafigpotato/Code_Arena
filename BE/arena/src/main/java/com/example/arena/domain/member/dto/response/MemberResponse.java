ef9cd8348cc06f01be086cd442e73ea69349fe58
이정혁 <gacomoria14@naver.com>
2024-07-15 14:04:40 +0900

    S11P11A807-28 fix: MemberResponse 수정
    Former-commit-id: 950ec7995f70295551ae958603ec17f011afbbf5

--- BE/arena/src/main/java/com/example/arena/domain/member/dto/response/MemberResponse.java
+++ BE/arena/src/main/java/com/example/arena/domain/member/dto/response/MemberResponse.java
@@ -5,10 +5,12 @@ import lombok.Builder;
 import lombok.Getter;
 
 import java.util.Date;
+import java.util.UUID;
 
 @Getter
 @Builder
 public class MemberResponse {
+    private UUID id;
     private String email;
     private String name;
     private String nickname;
