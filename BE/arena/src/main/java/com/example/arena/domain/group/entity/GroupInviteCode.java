ba1d78321e8c950d094c3a75e22d81e38aaa1b2a
진주원 <drasgon@naver.com>
2024-07-15 12:57:50 +0900

    [FEAT : BE S11P12A807-70] Group과 Member간 연관관계 설정 및 엔티티 속성 추가
    Former-commit-id: e0bfe5432e122d0142555e6570550349ffbeb01b

---
+++ BE/arena/src/main/java/com/example/arena/domain/group/entity/GroupInviteCode.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:2a707179a9a8e6f37b7820ed22e99fdd03def41e2f14ad30d153cfb13991c5e4
+size 108
---
+++ BE/arena/src/main/java/com/example/arena/domain/group/entity/GroupMember.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:788cde511c6eeafcb74573ca08acdd75150eea3e29a62011a1fb826456d7b0eb
+size 1120
