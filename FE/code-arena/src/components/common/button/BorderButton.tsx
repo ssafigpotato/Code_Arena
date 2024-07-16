b84f5b2c07c231936d8314b817c8b280065d0180
inception010203 <inception010203@gmail.com>
2024-07-16 16:51:55 +0900

    S11P12A807-100 refactor: 버튼 컴포넌트 리팩토링 및 랜딩 페이지 리팩토링
    Former-commit-id: 38c5b31fbed0cde359d6bc743b261cd3f71564fc

--- FE/code-arena/src/components/common/button/BorderButton.tsx
+++ FE/code-arena/src/components/common/button/BorderButton.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:12b68be1cdca1a1580d2e0168702503169962148fec81a1171ca8059dff19768
-size 715
+oid sha256:ef623b4e0008b569a4bb1e54cabed876be645f2687cc121ae384d2fb44b9ea11
+size 1135
