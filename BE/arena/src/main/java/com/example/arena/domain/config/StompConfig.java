117e07f8da4f4eb7128be131da78b1fee763a02f
진주원 <drasgon@naver.com>
2024-07-16 17:25:28 +0900

    [FEAT : BE S11P12A807-54] RabbitMQ STOMP
    Former-commit-id: b30d54b35e39b65c5641df090e39100381ba313a

--- BE/arena/src/main/java/com/example/arena/domain/config/StompConfig.java
+++ BE/arena/src/main/java/com/example/arena/domain/config/StompConfig.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4dc41f424b4ed237b8a8c43b3b5f1255fa2eed8b8897db1922119db11d0f69aa
-size 2244
+oid sha256:75db0a59c6b6cb0c3870bd67717f36f102aef430477e0f3d941046438b4bfb1d
+size 2149
