e2d36f50338022b9b32771521b086b6019bc7236
Dohyungh <bley1217@gmail.com>
2024-07-16 17:07:39 +0900

    S11P12A807-30 feat: buttonText 속성에 null이 추가 가능하도록 수정
    Former-commit-id: 720a18c9feb53c5111ea707b291e1fc3a91cf70d

--- FE/code-arena/src/components/common/button/BorderButton.tsx
+++ FE/code-arena/src/components/common/button/BorderButton.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:12b68be1cdca1a1580d2e0168702503169962148fec81a1171ca8059dff19768
-size 715
+oid sha256:4a36d8694de110c0afae70ba4c14c6d29e9c86ab1728e63a53c9f7e3e9609963
+size 740
