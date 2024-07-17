e7510c0a9111cf2195e8f9f534942885c4e8633d
허동민 <gjehdals456@gmail.com>
2024-07-17 10:19:41 +0900

    S11P12A807-107 fix : url 수정 및 yaml파일 수정 - done : / url 삭제 - done : yaml 파일 수정
    Former-commit-id: 0e0ad36421189249486ba8470407cda632a3c5a2

--- BE/arena/src/main/java/com/example/arena/domain/community/controller/BoardController.java
+++ BE/arena/src/main/java/com/example/arena/domain/community/controller/BoardController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:779c4f0df52a4921eb2acb208b161032c951767b12a71a2ce3fb940777c28d24
-size 3434
+oid sha256:eb67121733dbf50dc080debed39c6ff41819f050241fdbabeaba77845c6eddb0
+size 3431
