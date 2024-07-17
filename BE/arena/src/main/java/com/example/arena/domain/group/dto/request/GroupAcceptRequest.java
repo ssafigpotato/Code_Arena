f0689e81a65dfe21ea5027bfdaa5be4b8481f1e2
Dohyungh <bley1217@gmail.com>
2024-07-17 09:13:28 +0900

    S11P12A807-111-test2 Chore: test
    Former-commit-id: 9a99460e9fc78a8dfc24a4f9035446e4e5dabf48

--- BE/arena/src/main/java/com/example/arena/domain/group/dto/request/GroupAcceptRequest.java
+++ BE/arena/src/main/java/com/example/arena/domain/group/dto/request/GroupAcceptRequest.java
@@ -1,3 +1,14 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:30d072dfc9b61e6b884d16d7e42b3ddc6b0cdaebb71cc46f19dfa3d266a3dbe3
-size 276
+package com.example.arena.domain.group.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class GroupAcceptRequest {
+    private UUID groupId;
+}
