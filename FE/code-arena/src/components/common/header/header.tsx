accd34acc78b55ec8196b22f82dce3fe224b3a52
inception010203 <inception010203@gmail.com>
2024-07-17 17:56:08 +0900

    S11P12A807-97 feat: 커뮤니티 드롭박스 생성(수정 필요)
    Former-commit-id: b01e90f61a981d5d839d487a3be8d5f4ccf7b5ac

--- FE/code-arena/src/components/common/header/header.tsx
+++ FE/code-arena/src/components/common/header/header.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a7fb36609bda0c7fc90d5d69cfe08d3f926018a021433b4f41c452ad02bde1f2
-size 3288
+oid sha256:c05ed11852842202f8dc6094ee1051751e2034bc77071144f4615de5b6a6a6c1
+size 5837
