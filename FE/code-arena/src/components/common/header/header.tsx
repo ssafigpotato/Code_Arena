f42dd2d6b84b5fc4657782f5c04f21ffc87639be
inception010203 <inception010203@gmail.com>
2024-07-18 10:45:37 +0900

    S11P12A807-97 fix: 드롭메뉴박스 바깥을 클릭했을 때도 메뉴가 닫히도록 수정
    Former-commit-id: f9c58a12ac2813656431d2e0a7b331476e4a4975

--- FE/code-arena/src/components/common/header/header.tsx
+++ FE/code-arena/src/components/common/header/header.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c05ed11852842202f8dc6094ee1051751e2034bc77071144f4615de5b6a6a6c1
-size 5837
+oid sha256:5d539e35b9bf3da0aae1e3a1ecf0ebde6d6f461f879b67191038436b7fa8f87a
+size 6291
