380ed54eccf5a3c71c9e0c3daacc6a780bf0c493
Dohyungh <bley1217@gmail.com>
2024-07-18 17:56:09 +0900

    S11P12A807-98 feat: 그룹상세 페이지 그룹장 화면 작성중
    Former-commit-id: 564f5489d87c96b50c8ff49f1de0e550470c203e

--- FE/code-arena/src/app/page.tsx
+++ FE/code-arena/src/app/page.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c534454bd79e1ef35021d53ac66dd82264acfb32672ed2c21261dbe8b2e2c740
-size 4857
+oid sha256:c8dc1b289e73c4f2f7055eb058fdfa439907ac93242b507f6a02e6f43696de9b
+size 4838
--- FE/code-arena/src/styles/GlobalStyle.tsx
+++ FE/code-arena/src/styles/GlobalStyle.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c0336459766475e09abcac5eedd3c8532aff76af5353e4469f518c73a6c65d8a
-size 625
+oid sha256:9e86fa68abbaaf72cfead6f1c45689b0cfe6987892d1c56abb9c6c38d69f3c44
+size 608
