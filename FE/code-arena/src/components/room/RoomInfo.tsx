0fe45a6ff7b57b291fd64fc9a76303da32d3617e
inception010203 <inception010203@gmail.com>
2024-07-18 17:54:46 +0900

    S11P12A807-77 feat: room 컴포넌트 생성
    Former-commit-id: db3130a2de1288e072ac17533e474786dc2a5fe8

---
+++ FE/code-arena/src/components/room/RoomInfo.tsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:72b3b2e9c18486abcdba2ef1ade541ea471dfd96d9bf5621de74ff84a325c376
+size 1229
---
+++ FE/code-arena/src/components/room/RunCode.tsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:1029de44ed325b76df27fa03b784b390948a049547c2597ee3ddd960a95e0302
+size 198
