4b6a1ee544402260f114a2b4eb9f4057fd3c3d50
inception010203 <inception010203@gmail.com>
2024-07-18 15:54:50 +0900

    S11P12A807-77 hotfix: 헤더 position을 sticky로 수정
    Former-commit-id: 51f58b65f51f93f034b9d378af725969e1cb697b

--- FE/code-arena/src/app/room/page.tsx
+++ FE/code-arena/src/app/room/page.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0dae4c03de252bb71c975c40ede0c450b6cb8d0e091b0516d2b53662ed72da57
-size 104
+oid sha256:94c8f84ae51f1a870f7b08b23959827ee32860e7c87b54b61ccb993d34c0216e
+size 287
