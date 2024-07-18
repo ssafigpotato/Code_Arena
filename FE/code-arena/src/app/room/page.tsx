ef2f00b56915b658a3534e0879580f5473e8c421
inception010203 <inception010203@gmail.com>
2024-07-18 17:54:46 +0900

    S11P12A807-77 feat: room 컴포넌트 생성
    Former-commit-id: a69860e4ba7866729a950c04db225a76d846efb3

--- FE/code-arena/src/app/room/page.tsx
+++ FE/code-arena/src/app/room/page.tsx
@@ -1,17 +1,21 @@
 "use client";
 import React from "react";
 import styled from "styled-components";
+import RoomInfo from "@/components/room/RoomInfo";
 
 const Wrapper = styled.div`
   width: 100vw;
   // padding: 0.5rem;
   color: #ffffff;
+  * {
+    border: 1px solid lime;
+  }
 `;
 
 export default function Room() {
   return (
     <Wrapper>
-      <h1>room 화면입니다.</h1>
+      <RoomInfo />
     </Wrapper>
   );
 }
