05bc58ae26adfa0df30450cab7b1c236c8fdffd1
inception010203 <inception010203@gmail.com>
2024-07-18 13:35:11 +0900

    S11P12A807-77 feat: room 화면 생성(헤더, 푸터 보이지 않도록 처리)
    Former-commit-id: 15cfdcb6febb66c541142466b333e3baaf3aa6f7

--- FE/code-arena/src/app/room/page.tsx
+++ FE/code-arena/src/app/room/page.tsx
@@ -1,7 +1,3 @@
-export default function write() {
-  return (
-    <>
-      <h1></h1>
-    </>
-  );
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:0dae4c03de252bb71c975c40ede0c450b6cb8d0e091b0516d2b53662ed72da57
+size 104
