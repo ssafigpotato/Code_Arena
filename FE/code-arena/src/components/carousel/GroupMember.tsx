54485e54571205ed3f74a471c6aff1a33b6e2e7e
Dohyungh <bley1217@gmail.com>
2024-07-18 17:56:09 +0900

    S11P12A807-98 feat: 그룹상세 페이지 그룹장 화면 작성중
    Former-commit-id: d4cc7f9e9daae4738368923335cab97d7e515822

--- FE/code-arena/src/components/carousel/GroupMember.tsx
+++ FE/code-arena/src/components/carousel/GroupMember.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2b2ad8c6ddff83bd818e8368727de19159670d1c3c531b7ec88a65dd42bfd4b7
-size 2970
+oid sha256:2dad29a81f137e855a3425823dec10eff8b2ac1610280a27b59f9507d937b7ec
+size 3507
--- FE/test.md
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:1e52bd986c2329a0afc37548ea04e6f1e48d0f74c1845b1d312288db3ee55793
-size 71
