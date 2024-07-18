6d9b9a7c55ec0ede6c139f6819b0c85a3408f2d4
inception010203 <inception010203@gmail.com>
2024-07-18 15:54:50 +0900

    S11P12A807-77 hotfix: 헤더 position을 sticky로 수정
    Former-commit-id: 7eb73fbb1d3ebafb5d22d16ddecc45fbb6147687

--- FE/code-arena/src/app/room/page.tsx
+++ FE/code-arena/src/app/room/page.tsx
@@ -1,7 +1,17 @@
-export default function write() {
+"use client";
+import React from "react";
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+  width: 100vw;
+  // padding: 0.5rem;
+  color: #ffffff;
+`;
+
+export default function Room() {
   return (
-    <>
+    <Wrapper>
       <h1>room 화면입니다.</h1>
-    </>
+    </Wrapper>
   );
 }
--- FE/code-arena/src/styles/GlobalStyle.tsx
+++ FE/code-arena/src/styles/GlobalStyle.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c0336459766475e09abcac5eedd3c8532aff76af5353e4469f518c73a6c65d8a
-size 625
+oid sha256:664c488e35d9f84f24e1f35fba480f2ca9042cb917093a38621d2e0e89f1f041
+size 631
