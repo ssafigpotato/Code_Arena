14a4c1ce63eb48a528229bbf10a651700dc2d2e3
허동민 <gjehdals456@gmail.com>
2024-07-19 15:38:55 +0900

    S11P12A807-33 fix : queue 못 찾는 에러 해결 완료 -done : .env의 queue.name 사용
    Former-commit-id: 993ae74be0294d84f5b8d7b909f534d87a9491ec

--- BE/arena/src/main/java/com/example/arena/domain/code/service/RabbitMQService.java
+++ BE/arena/src/main/java/com/example/arena/domain/code/service/RabbitMQService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b78cbf07a6fad7a5b0b7e7baaeb7265b1324c735cb7ebf2cf8d40d12720dfa0a
-size 680
+oid sha256:d69eaaf873a80bc398266febaefa86f0244f5b36410cd8d427eb4774bd2669e6
+size 844
