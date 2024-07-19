a6e2d72bcc64cd4fd1d0085de4b918c947e18eae
Dohyungh <bley1217@gmail.com>
2024-07-19 10:19:33 +0900

    S11P12A807-98 feat: 그룹상세 페이지 완성
    Former-commit-id: 13d005c68b0e0d9a97536d74ade57e012bcd7ecb

--- FE/code-arena/src/app/groupdetail/[id]/page.tsx
+++ FE/code-arena/src/app/groupdetail/[id]/page.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bafdcb67844775a6dbbd16674c0f933d2111e8f428322d95f99849d072b0f7ee
-size 12665
+oid sha256:00f13fe0d4e261de7bd5c7f211450023c95c83d661a464ac3d683a30f9937903
+size 12735
