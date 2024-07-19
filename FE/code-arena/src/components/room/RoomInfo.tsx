ecf86e9e190cae7d6129cea458b9075cf77b97e5
inception010203 <inception010203@gmail.com>
2024-07-19 12:57:12 +0900

    S11P12A807-77 feat: RoomInfo의 라이브 관련 정보컴포넌트 생성
    Former-commit-id: 621ccb7a12252f5287ab427f918fea15e6d4aabc

--- FE/code-arena/src/components/room/RoomInfo.tsx
+++ FE/code-arena/src/components/room/RoomInfo.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:72b3b2e9c18486abcdba2ef1ade541ea471dfd96d9bf5621de74ff84a325c376
-size 1229
+oid sha256:8de22e9b70d4a067c60f6d3e5e108d4e6bfa45779cf5815cb36f9ab1cbb687f4
+size 4777
