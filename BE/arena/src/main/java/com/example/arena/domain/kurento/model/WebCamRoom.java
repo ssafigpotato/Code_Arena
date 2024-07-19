411f64c5e7562b594468997264b85c3a83759193
진주원 <drasgon@naver.com>
2024-07-19 17:06:21 +0900

    [FEAT : BE S11P12A807-45] WebCamRoom Management
    Former-commit-id: d7c260f1785403b40869ff8a473dc5aa44a531e6

--- BE/arena/src/main/java/com/example/arena/domain/kurento/model/WebCamRoom.java
+++ BE/arena/src/main/java/com/example/arena/domain/kurento/model/WebCamRoom.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:03f711e0042a2df2462a18e4bbafb8dec1c53ef07267d914bd8c841dc19c918f
-size 3255
+oid sha256:70f2831df3bf8ded87591e0bd58045fdeb3b37f3e112f6ec9212006eba88eca8
+size 3690
