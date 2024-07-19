09169e20df09bdc0b7004a328743f7e88c74b2ec
허동민 <gjehdals456@gmail.com>
2024-07-19 17:52:39 +0900

    S11P12A807-132 feat : 커뮤니티 상세 검색 일부 구현완료 - done : 옵션에 따른 검색 결과 출력 - need : 테스트필요 및 에러 코드 수정 필요.
    Former-commit-id: 2eb0527986b3eafc08f40cd22312424ffb736803

---
+++ BE/arena/src/main/java/com/example/arena/domain/community/dto/response/SearchBoardResponse.java
@@ -0,0 +1,19 @@
+package com.example.arena.domain.community.dto.response;
+
+import java.util.Date;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+// Response : 제목 / 닉네임 / 작성일 / 추천 / 댓글 수
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class SearchBoardResponse {
+	private String title;
+	private String nickname;
+//	private Date createdAt; 구현 필요
+	private int likes;
+	private int comments;
+}
--- BE/arena/src/main/java/com/example/arena/domain/community/service/BoardService.java
+++ BE/arena/src/main/java/com/example/arena/domain/community/service/BoardService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:50b3ac03ca23292d8219ee97b0ea734f8db02e956dd8b1ed39735fbfa6eaf85d
-size 5278
+oid sha256:e4ad80f68487e6b86721c77f75b78a76c89db8ac7f09fb2ac269acf483e02160
+size 6600
