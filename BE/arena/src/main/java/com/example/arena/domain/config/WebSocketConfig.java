c741cd6ffa00d48bc72f67a7ba2b20939fd73f3d
진주원 <drasgon@naver.com>
2024-07-19 13:29:38 +0900

    [FEAT : BE S11P12A807-45] WebCamSession
    Former-commit-id: 2c2b7a8cc9b55d7b0653e273430409ba5f16eee8

---
+++ BE/arena/src/main/java/com/example/arena/domain/config/WebSocketConfig.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:052fc18e8989d9f9a6c2bcfe2fbf448a64b7cf7bd17591a79172e348c6b49c3d
+size 535
---
+++ BE/arena/src/main/java/com/example/arena/domain/kurento/model/WebCamRegistry.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:65faf822ebe6b664d556a6ea7ce11688f0d6356dbbcac18aca4e8143e012788b
+size 1383
