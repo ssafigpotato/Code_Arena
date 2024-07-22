17998c7ccb9749cf6a97c78d8ecc61c341a3f234
ryucream612 <ryucream612@naver.com>
2024-07-22 13:08:02 +0900

    S11P12A807-31 feat: 검색 더보기 버튼 처리, 라우팅 경로 변경
    Former-commit-id: f561ac014bd8a9e20a336e146ed247f88775ec3d

---
+++ FE/code-arena/src/app/community/[board]/page.tsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:ed6980d7950167e879d8dddbb2dc8072b204d81b0a5f2056432e4b651cea1363
+size 4234
--- FE/code-arena/src/app/search/page.tsx
+++ FE/code-arena/src/app/search/page.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c32b8a1fb0483a711713d6a881c812869c9273de6aaaf541c0dc27ddd9becf53
-size 17787
+oid sha256:5109ca154501955a7333888db68de7800836e5411214dbc8f33d6cf4e25a7244
+size 18522
