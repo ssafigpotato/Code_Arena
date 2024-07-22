e08108ee807c079fead6c264272549e7d46e6edc
inception010203 <inception010203@gmail.com>
2024-07-22 10:54:24 +0900

    S11P12A807-77 fix: /room/[id] (숫자) 형태일 때만 헤더와 푸터가 사라지도록 정규표현식으로 수정
    Former-commit-id: 94ed30270c028d0456fab44e9612f2518f69816c

--- FE/code-arena/src/app/layout.tsx
+++ FE/code-arena/src/app/layout.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cded46eb82d0e4ead92cab55d121fb9eb4706378f743cecfa41bbc0ef7936c8b
-size 1395
+oid sha256:88fb17511a7aebbd2d9d2a59d3e142114c112fadcb285fc876852a500edc1655
+size 1320
