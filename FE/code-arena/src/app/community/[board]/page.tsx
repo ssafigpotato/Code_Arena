e5aa237867bba1e6d1cc2098459a25770cb4cc69
ryucream612 <ryucream612@naver.com>
2024-07-22 13:48:34 +0900

    S11P12A807-31 feat: 에러 페이지 추가
    Former-commit-id: 4b346b9ac909849ec6b3bb21880e581d7f260e0e

--- FE/code-arena/src/app/community/[board]/page.tsx
+++ FE/code-arena/src/app/community/[board]/page.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ed6980d7950167e879d8dddbb2dc8072b204d81b0a5f2056432e4b651cea1363
-size 4234
+oid sha256:e33344b8e810a159c1496fe4c6754a8fd5ecd9f41ebcd690e4858111dc112b1c
+size 4370
