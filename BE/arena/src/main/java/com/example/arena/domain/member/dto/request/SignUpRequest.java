f5aca560281dcc1afede51f247d1802555eee5ac
glenn.syj <glenn.syj@gmail.com>
2024-07-22 12:56:32 +0900

    S11P12A807-155 feat: 회원 도메인 엔티티 및 DTO 수정
    - done: 누락된 닉네임 주기 속성 추가
    - done: 엔티티 접근 범위 변경
    - done: DTO 검증 로직 추가
    
    
    Former-commit-id: e50a246dd2f2610708b9fd25ffc14618bdf891a3

--- BE/arena/src/main/java/com/example/arena/domain/member/dto/request/SignUpRequest.java
+++ BE/arena/src/main/java/com/example/arena/domain/member/dto/request/SignUpRequest.java
@@ -1,21 +1,3 @@
-package com.example.arena.domain.member.dto.request;
-
-import com.example.arena.domain.member.entity.Role;
-import lombok.*;
-
-import java.util.Date;
-
-@Getter
-@Builder
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
-public class SignUpRequest {
-    private String email;
-    private String password;
-    private String name;
-    private String nickname;
-    private Date birth;
-    private Role role;
-    private String image;
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:738d286223508edb6cd67b543de7a49a758b45ff69fb4bdedac21b2f37412a56
+size 1098
--- BE/arena/src/main/java/com/example/arena/domain/member/jwt/CustomLogoutFilter.java
+++ BE/arena/src/main/java/com/example/arena/domain/member/jwt/CustomLogoutFilter.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:723debd4114db317b9becf73e22739e180c192df66293dd871659b077c3bd541
-size 3492
+oid sha256:0f30a0a3f09c9c673c7aa4050d3e401111b1bb41e307326bc129f302ba0a40fb
+size 3495
