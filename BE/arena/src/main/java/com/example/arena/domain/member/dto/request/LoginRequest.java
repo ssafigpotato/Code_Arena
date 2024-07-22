a81b6dff8f8a92c1436e7b177976046c17a74b06
glenn.syj <glenn.syj@gmail.com>
2024-07-22 12:56:32 +0900

    S11P12A807-155 feat: 회원 도메인 엔티티 및 DTO 수정
    - done: 누락된 닉네임 주기 속성 추가
    - done: 엔티티 접근 범위 변경
    - done: DTO 검증 로직 추가
    
    
    Former-commit-id: b45c0fcad677255ed888926184d9ec20a816bf14

--- BE/arena/src/main/java/com/example/arena/domain/member/dto/request/LoginRequest.java
+++ BE/arena/src/main/java/com/example/arena/domain/member/dto/request/LoginRequest.java
@@ -1,11 +1,3 @@
-package com.example.arena.domain.member.dto.request;
-
-import lombok.Builder;
-import lombok.Getter;
-
-@Getter
-@Builder
-public class LoginRequest {
-    private String email;
-    private String password;
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:71391ef3e8a5486fef24037f8a4e0e6fa905746759fc01eb845d8a5180128369
+size 778
