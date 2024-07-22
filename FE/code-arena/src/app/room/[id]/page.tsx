2a90058a5d084a6131432b1787b02a7be7f6e246
inception010203 <inception010203@gmail.com>
2024-07-22 13:07:19 +0900

    S11P12A807-166 feat: 룸 페이지 메인 컴포넌트 생성(문제, 코드 편집기/터미널, 화상회의 화면)
    Former-commit-id: a72fb9526335dcc06b7af628f91bc06f2ee321c3

--- FE/code-arena/src/app/room/[id]/page.tsx
+++ FE/code-arena/src/app/room/[id]/page.tsx
@@ -1,21 +1,3 @@
-"use client";
-import React from "react";
-import styled from "styled-components";
-import RoomInfo from "@/components/room/RoomInfo";
-
-const Wrapper = styled.div`
-  width: 100vw;
-  // padding: 0.5rem;
-  color: #ffffff;
-  * {
-    border: 1px solid lime;
-  }
-`;
-
-export default function Room() {
-  return (
-    <Wrapper>
-      <RoomInfo />
-    </Wrapper>
-  );
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:fa6280228dbb0063eec648b1cc45e09f6666d3c0e08b9367f2e66d4068a9681b
+size 834
--- FE/code-arena/src/components/room/Question.tsx
+++ FE/code-arena/src/components/room/Question.tsx
@@ -2,3 +2,13 @@ import React, { useState, useRef, useEffect } from "react";
 import styled from "styled-components";
 import BorderButton from "@/components/common/button/BorderButton";
 import Link from "next/link";
+
+const Question = () => {
+  return (
+    <>
+      <h1>문제 div</h1>
+    </>
+  );
+};
+
+export default Question;
