47f3211b51772068aa5a923a90edd411a6dc1897
inception010203 <inception010203@gmail.com>
2024-07-23 10:04:43 +0900

    S11P12A807-167 feat: 헤더에 로그인과 회원가입 링크 연결
    Former-commit-id: 0b44d0b1525f7515a9f8212151da7cfc0775173f

--- FE/code-arena/src/components/common/header/header.tsx
+++ FE/code-arena/src/components/common/header/header.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:121fd2c12314f588bd68f11418b911158854cea537143e6dd2644098eef3301e
-size 6994
+oid sha256:2750bbce759dedaf3ddef28a2a3b3f5e16abbadf25907f59732bf8c45af47dcb
+size 7225
