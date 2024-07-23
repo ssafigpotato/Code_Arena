49b97622c5f2782840f409e7d63f95c170b47a5e
inception010203 <inception010203@gmail.com>
2024-07-23 10:09:55 +0900

    S11P12A807-167 feat: 헤더에 각 커뮤니티 게시판(그룹 모집, 질문, 피드백) 링크 연결
    Former-commit-id: 7406138ed776ecd9acfaa1c66d6bfa87c1a00cf7

--- FE/code-arena/src/components/common/header/header.tsx
+++ FE/code-arena/src/components/common/header/header.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2750bbce759dedaf3ddef28a2a3b3f5e16abbadf25907f59732bf8c45af47dcb
-size 7225
+oid sha256:6744fd312245584fc8ef265fd0c2d661d34b172d539a886f5aaaccc961bb8adc
+size 7375
