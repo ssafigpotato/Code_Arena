c29523700da086139db4e7bd87d915deac047450
ryucream612 <ryucream612@naver.com>
2024-07-23 10:22:27 +0900

    S11P12A807-34 feat: 라이브 종료 페이지 응시자 마크업
    Former-commit-id: 64d0b4eea5dd59ae7da132ae0a26b151b517684e

--- FE/code-arena/src/app/room/[id]/end/page.tsx
+++ FE/code-arena/src/app/room/[id]/end/page.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ab8e1e32f6de01c9ed314b53f1e312f0aecbdea98bf536bd8b84fa0bb11f034e
-size 317
+oid sha256:704607c6835cb7dd386bc892ff4f9719fc96e3da012edc4323b05d01cee13755
+size 2882
