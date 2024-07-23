083c3557cf0cd0ae22dc12219d2b0831e34b7a9c
ryucream612 <ryucream612@naver.com>
2024-07-23 16:09:55 +0900

    S11P12A807-114 feat: write 코드 boardstore 사용하도록 변경
    Former-commit-id: 3e1ffc2f3271ec7ef2b16109925b72f4e612cbed

--- FE/code-arena/src/components/board/write/WriteContainer.tsx
+++ FE/code-arena/src/components/board/write/WriteContainer.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f25833a8c0d7862c49c0ab61ee5eb6f3719993fed324937be545376835a4ac4d
-size 4711
+oid sha256:9160878757742fbe4fa52360dcbb3ebf73e63201b5a1e818a2f85d3c4982af40
+size 4600
