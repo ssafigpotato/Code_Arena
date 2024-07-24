b97b43cef6e0197a50b755323525cf3972a1c2f5
허동민 <gjehdals456@gmail.com>
2024-07-24 12:11:49 +0900

    S11P12A807-170 feat : BaseEntity 구현과 그에 따른 created 내림차순 정렬 - done : BaseEntity 구현 - done : Board Entity extends 구현 - done : JPA createdAt 내림차순 구현 - need : Swagger Test
    Former-commit-id: 0a0b4b22eb27eaf36cb72249ee6c27012bfdb9d7

--- BE/arena/src/main/java/com/example/arena/domain/community/entity/Board.java
+++ BE/arena/src/main/java/com/example/arena/domain/community/entity/Board.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d21f6cfcb80eb338e9c49ff746518a5f14a3e1b911e1620a785b0690bdf85c6f
-size 1713
+oid sha256:18f991ed41b9298bdb01d68da5377b7ebe50a9a79d8bbdef9f1a5a4c727d4217
+size 1731
