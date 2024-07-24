82895c71c357405b172f3f4b963cfbfa5ba9bc24
inception010203 <inception010203@gmail.com>
2024-07-24 16:10:53 +0900

    S11P12A807-181 fix: carousel 정렬 css 수정
    Former-commit-id: bcc319847055bbe24d5d620b46e5191097ab6245

--- FE/code-arena/src/components/mypage/MyInfos.tsx
+++ FE/code-arena/src/components/mypage/MyInfos.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:35e4ee3e0e8d429c3c04f5fb87f7adb05aa7e32356ccc9d7c74f60beda234af4
-size 8454
+oid sha256:a7e513af835aabdc0b06c4d8227d777846def38ed7560c88bbc46530eafe2971
+size 9332
