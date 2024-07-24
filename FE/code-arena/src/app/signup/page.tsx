6dbd8982354c2e2be03508703fa18f9d8c81c497
Dohyungh <bley1217@gmail.com>
2024-07-24 14:06:52 +0900

    S11P12A807-121 feat: 회원가입 POST 시험 완료
    Former-commit-id: 093ae062b5a91c5fb4e83c36755f3b61f516f567

--- FE/code-arena/src/app/signup/page.tsx
+++ FE/code-arena/src/app/signup/page.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:98ce40ee5f7ca0a6c51ce3c2f451ef461be76cf664a074127a9f93e6e4b0e83a
-size 6697
+oid sha256:dd9866b1c3f288f70585acbd2b676c90fabbb01455f1db0686895d6605574b23
+size 8041
