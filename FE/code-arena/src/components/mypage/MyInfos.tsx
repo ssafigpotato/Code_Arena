a379e790d6d7808985de38d26bc307614e26c77c
inception010203 <inception010203@gmail.com>
2024-07-24 11:03:19 +0900

    S11P12A807-181 feat: 마이페이지의 myinfo 컴포넌트 기본 정보 구현
    Former-commit-id: c1c1e51a22d82fa7f697f289a4bf7a225f85af2f

--- FE/code-arena/src/components/mypage/MyInfos.tsx
+++ FE/code-arena/src/components/mypage/MyInfos.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:543e4836e04f61ae4a633f7f1457a02c6809b58d85ee16d688d21cae2b9b447b
-size 617
+oid sha256:96a785517d74889472672f7bc8fe52c4e680be6364c39684729dcbf4f40e8816
+size 3300
