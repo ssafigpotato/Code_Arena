e098c8c633ca4fcde6e8669d02de788a90d2c237
Dohyungh <bley1217@gmail.com>
2024-07-24 10:22:29 +0900

    S11P12A807-121 Fix: 그룹상세페이지 제목 수정
    Former-commit-id: d1744092b89ce28d7a6162b5a06849d4fc404d62

--- BE/arena/src/main/java/com/example/arena/domain/kurento/model/KurentoStatus.java
+++ BE/arena/src/main/java/com/example/arena/domain/kurento/model/KurentoStatus.java
@@ -1,3 +1,5 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:91331ec6a021c2403c6638b0db123ff654f31b83c38a01b8988d1f5663e19ecd
-size 80
+package com.example.arena.domain.kurento.model;
+
+public enum KurentoStatus {
+
+}
