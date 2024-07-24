6532dc32fc6a13fe28f01c6bf3b15b30318b2935
ryucream612 <ryucream612@naver.com>
2024-07-24 13:05:54 +0900

    S11P12A807-106 feat: 라이브 종료 페이지 면접관 마크업
    Former-commit-id: d032a6602afb9d7f67363912fe535836a48ec3d5

--- BE/arena/src/main/java/com/example/arena/domain/room/repository/RoomRepository.java
+++ BE/arena/src/main/java/com/example/arena/domain/room/repository/RoomRepository.java
@@ -1,3 +1,11 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:aba1d51ce2d574eb0969a08e52e03fe890db187a73b0b0a6e12c56cb31143292
-size 260
+package com.example.arena.domain.room.repository;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.arena.domain.room.entity.Room;
+
+public interface RoomRepository extends JpaRepository<Room, UUID> {
+
+}
