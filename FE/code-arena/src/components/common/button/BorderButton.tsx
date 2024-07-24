a45da01bfbc4f4387ba5d7faa4bd56aa24e3dc44
ryucream612 <ryucream612@naver.com>
2024-07-24 17:43:11 +0900

    S11P12A807-187 feat: BorderButton의 onClick type 수정
    Former-commit-id: 6ebf922daf8bb480a8fa62e07e2c28f30f6b6c80

--- FE/code-arena/src/components/common/button/BorderButton.tsx
+++ FE/code-arena/src/components/common/button/BorderButton.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:14aa14afd08a9edb51d52ddcef200065cabd5ea2ae051b961b8807a2bf8bb96d
-size 1439
+oid sha256:cac02f64f75a85d35957d7c4148bdc6bb9882bc11f1599c66da66e898aa87500
+size 1484
