88c85394f5115cf9672461950367e735911e4d59
허동민 <gjehdals456@gmail.com>
2024-07-24 17:47:13 +0900

    S11P12A807-170 feat : 다른 카테고리 검색에도 날짜 내림차순으로 정렬하여 보냄 - done : title content both - need : swagger test
    Former-commit-id: 89962994d3781f078f9a667ebe6ff5a3ae5057d4

--- BE/arena/src/main/java/com/example/arena/domain/community/repository/BoardRepository.java
+++ BE/arena/src/main/java/com/example/arena/domain/community/repository/BoardRepository.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:52c73c4a7e2e4afc32d71294364467f1bd295fb0b5cc19d29ad4a85f6e1206f9
-size 1483
+oid sha256:1d03343a703fedd418a63143a3bbd07c1c9d5fc57e23146264e472f4f125813d
+size 1686
