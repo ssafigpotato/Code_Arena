5d02ddce7f4243ca44ee8951cfbeb8d2c855a1f7
허동민 <gjehdals456@gmail.com>
2024-07-25 17:29:29 +0900

    S11P12A807-170 fix : 게시판 경로 못 찾던 버그 fix
    Former-commit-id: af63e17a611af6b0eebe79d0f494b83cd3e34dbe

--- FE/code-arena/src/store/boardstore.tsx
+++ FE/code-arena/src/store/boardstore.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b46aff228bc6dfdb406e664ed2eec45d60bbe429fde995dd5743e35d9d9a0d28
-size 1714
+oid sha256:5e7b6ec8096494f77b45bb04c5626deb5df0aa76c2c4031c009c2ecc716eeb1a
+size 2265
