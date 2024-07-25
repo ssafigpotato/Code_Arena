726f5d09e0b09f5ebcbabcf78a23e95c0954abdf
inception010203 <inception010203@gmail.com>
2024-07-25 13:06:00 +0900

    S11P12A807-188 fix: 더미데이터의 닉네임을 mypage로 임시 변경하고, Pass Rank의 위치를 수정(부모컴포넌트에 relative 설정)
    Former-commit-id: 1973351471ca7c5228c98727a0dd1f371593781c

--- FE/code-arena/src/components/mypage/Pass.tsx
+++ FE/code-arena/src/components/mypage/Pass.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d9f04b2ac50d1dea99e7e4ff0b91365a0b99e69bcb2b4bb258f1b57a8f165b8f
-size 3196
+oid sha256:4414e99f08675c7a8a8ffb12286caaca494634990477a38fd2b7548339ee1de1
+size 3220
