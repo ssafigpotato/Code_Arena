fcfe3bc8db2c38ce2914dc823340cfaee8f64478
inception010203 <inception010203@gmail.com>
2024-07-25 16:48:48 +0900

    S11P12A807-192 fix: 면접관/응시자 포지션을 바꿀 수 없도록 div로 수정하고, 응시자가 시작하기 버튼을 누를 수 있도록 수정
    Former-commit-id: bafb4d8cfeeba826a0f3e9886d4ac6282a3f05ed

--- FE/code-arena/src/components/room/RoomInfo.tsx
+++ FE/code-arena/src/components/room/RoomInfo.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6bea8a7d1c9e1d8607e66e491da211768c2d3d5f825105ed0e87ae6174d4baca
-size 6785
+oid sha256:2cd1ea3d7ba2ace1e7d2afacc1cde8dcc1d6729f310f60be73be96fd2c1fcab1
+size 6956
