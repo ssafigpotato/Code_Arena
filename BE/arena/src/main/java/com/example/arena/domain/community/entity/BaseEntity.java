ec9cd4e8384b3ac170b5585d64416b770f6a2816
허동민 <gjehdals456@gmail.com>
2024-07-25 11:09:31 +0900

    S11P12A807-170 feat : BaseEntity에 soft delete 구현 완료 - done : soft delete를 구현하여 db 직접 삭제가 아닌 detele 변수를 건들임 - need : BoardLike에 접목 시키면 좋을 것으로 예상
    Former-commit-id: 09d3d8e607e205698a7b8dbae40645c47817a58d

--- BE/arena/src/main/java/com/example/arena/domain/community/entity/BaseEntity.java
+++ BE/arena/src/main/java/com/example/arena/domain/community/entity/BaseEntity.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:976e53e9eb264016657c31ad72547f86ec9eb14eb7dbb5a3c7d11fa3b1fc16db
-size 665
+oid sha256:b15e1c1a6e9b243ff7984ea341817aa06a2f780791767d69d55b8665ffec2ee9
+size 1026
