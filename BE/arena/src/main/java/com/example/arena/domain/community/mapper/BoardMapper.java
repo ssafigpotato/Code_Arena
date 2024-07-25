e89077df28927a5121588cfa423bd93c8934840b
허동민 <gjehdals456@gmail.com>
2024-07-25 14:00:06 +0900

    S11P12A807-1700 feat : 게시글 리스트 조회시 댓글까지 반환하게 기능 추가 - done : 댓글 수 가져오기 - done : 그에 따른 dto 추가와 comment 기본 설정 변경 - need : swagger test 필요
    Former-commit-id: 560da9cb4ef570cfa1e2d6f9d98aa956e28bda7a

--- BE/arena/src/main/java/com/example/arena/domain/community/mapper/BoardMapper.java
+++ BE/arena/src/main/java/com/example/arena/domain/community/mapper/BoardMapper.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0352ce46aa75909f0eac749cd0168e454434f1c908cd4ac9ef0e6ff3d689d6f3
-size 1815
+oid sha256:d06a5cad073fb1dbf486d4e1e64e6001749480cde86053c1ff6f59c2e2e29c8d
+size 2062
