ceac7968f7fea8fba744adf299f971faf91e3a45
glenn.syj <glenn.syj@gmail.com>
2024-07-25 17:58:53 +0900

    S11P12A807-190 test: 회원 가입 테스트 코드 완성
    - done: 공통 bean 이용을 위한 MemberTestConfig 작성
    
    - need: Spring Security Test 적용
    
    
    Former-commit-id: 849a8569dcfea4c57bde2f67a9a252417b26bec1

--- BE/arena/src/test/java/com/example/arena/domain/member/controller/MemberControllerTest.java
+++ BE/arena/src/test/java/com/example/arena/domain/member/controller/MemberControllerTest.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:56da880f8ae0d6030ddaaf72fd43aa2e99c0623a9dcb0c60c683657e8b3642bc
-size 2448
+oid sha256:cefabd1c49f8c6797715b2192fc2552b56595914351e53509dc2b9ffe341d6bd
+size 2645
