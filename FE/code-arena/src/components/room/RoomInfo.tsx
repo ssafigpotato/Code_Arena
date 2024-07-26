9b3d00b952337edeb4ac1191574265e627b02ba6
inception010203 <inception010203@gmail.com>
2024-07-26 14:41:33 +0900

    S11P12A807-195 fix: 응시자가 종료하기 버튼을 클릭하면 테스트 경과 시간을 저장하고, 종료 페이지로 이동하도록 수정
    Former-commit-id: 356b9232b819beedaca153ab30c1f497feac9464

--- FE/code-arena/src/components/room/RoomInfo.tsx
+++ FE/code-arena/src/components/room/RoomInfo.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3a8931a2dfa24e55f868edbe4982f1415ed9b0268d851b1fa1b776974cc43ecb
-size 9596
+oid sha256:e11f24edd0e447662b5aed7c1a3aaea0108cdb55f30a1fe7a3f6853a19ceb69a
+size 10269
