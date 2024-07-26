ee173ba5ac2804a4045f01b2bcb10a6fbd46b85c
ryucream612 <ryucream612@naver.com>
2024-07-26 14:21:00 +0900

    S11P12A807-193 feat: 게시글 상세 화면에서 수정, 삭제 버튼 모달로 구현
    Former-commit-id: 6a38a688a93e777cb20fb88368615c1cfa04f05f

--- FE/code-arena/src/app/community/[category]/edit/[boardId]/page.tsx
+++ FE/code-arena/src/app/community/[category]/edit/[boardId]/page.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0cc0e83fcf17968e3e06eba053959c92619a853744b9648d9c3b87a490255ee8
-size 5924
+oid sha256:e30322fb77dfb7648483e1c4ac61e72a3950499ddd55fa7b5f715e789b21446a
+size 5982
