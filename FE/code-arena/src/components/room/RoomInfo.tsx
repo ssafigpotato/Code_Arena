9c60ac43a73bc379db746d117332296c459e88d5
inception010203 <inception010203@gmail.com>
2024-07-26 15:57:14 +0900

    S11P12A807-196 fix: useParams를 사용해, 테스트 종료 후 같은 id를 가진 종료 페이지로 이동할 수 있도록 수정
    Former-commit-id: 40f36b41bccecdcd65f82d326f0010cf654d9f79

--- FE/code-arena/src/components/room/RoomInfo.tsx
+++ FE/code-arena/src/components/room/RoomInfo.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:03cb9b5b783f5dbd507b1589aff0be0971d7a802a2f1ddf6e88ffa1d0b8055c4
-size 6956
+oid sha256:9c19b1ec7a01493db15c62161d55a4b9a8ffb92210d2a76bf25f702ea9d99141
+size 7036
--- FE/code-arena/src/components/room/roominfo-component/Timer.tsx
+++ FE/code-arena/src/components/room/roominfo-component/Timer.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3534eb74fee5f957949f6780e9c2dcffe22ddf98591b6e7e541e1b660fe5e4f6
-size 2705
+oid sha256:8827e730d5ff971637e138960b29ba5130676ca84aab94cd82a37eb0761a7a0f
+size 2787
