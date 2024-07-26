a77b837f6565481d5015b369725a1cff9381b252
inception010203 <inception010203@gmail.com>
2024-07-26 10:15:05 +0900

    S11P12A807-192 fix: 버튼의 border를 검은색으로 수정
    Former-commit-id: 1af15974decd572a077415cbde13025fe4f4bda4

--- FE/code-arena/src/components/room/RoomInfo.tsx
+++ FE/code-arena/src/components/room/RoomInfo.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5efde0f64434d0a51114cfdfa24710d87b72186e2a179cee304f2bb0902737be
-size 9506
+oid sha256:3a8931a2dfa24e55f868edbe4982f1415ed9b0268d851b1fa1b776974cc43ecb
+size 9596
