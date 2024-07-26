cdcb594842c703f18c9e5ae579a422c05d5ed061
inception010203 <inception010203@gmail.com>
2024-07-26 14:45:51 +0900

    S11P12A807-195 fix: 종료하기 버튼 클릭시 테스트 종료를 한 번 더 확인하는 경고창이 출력되도록 수정
    Former-commit-id: 2226cde8ac46214e5e343fce62a250e64f1f7408

--- FE/code-arena/src/components/room/RoomInfo.tsx
+++ FE/code-arena/src/components/room/RoomInfo.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e11f24edd0e447662b5aed7c1a3aaea0108cdb55f30a1fe7a3f6853a19ceb69a
-size 10269
+oid sha256:5f3c96f736052d160546447ddac14f2b2d75501d41a8afb1e8ebcb0fca40bc0f
+size 10361
