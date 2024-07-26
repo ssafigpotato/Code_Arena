a48b39f80d695b010132580410d4c182dc5da4e7
ryucream612 <ryucream612@naver.com>
2024-07-26 13:36:52 +0900

    S11P12A807-139 feat: 게시글 수정 axios 코드 작성
    Former-commit-id: 96912b2425b9dbd791845f7b865119c18c52bbd4

--- FE/code-arena/src/components/room/room-component/CodeEditor.tsx
+++ FE/code-arena/src/components/room/room-component/CodeEditor.tsx
@@ -1,14 +1,3 @@
-import React, { useState, useRef, useEffect } from "react";
-import styled from "styled-components";
-import BorderButton from "@/components/common/button/BorderButton";
-import Link from "next/link";
-
-const Editor = () => {
-  return (
-    <>
-      <h1>코드 편집기</h1>
-    </>
-  );
-};
-
-export default Editor;
+version https://git-lfs.github.com/spec/v1
+oid sha256:f39d594de155edc002d1b29c53e488ad0b1abd9e65ec842f2178704938cbc2e4
+size 312
