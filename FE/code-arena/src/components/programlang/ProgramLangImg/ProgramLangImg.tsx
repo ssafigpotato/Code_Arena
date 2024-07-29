f6546318b195d984d9eb7f152442d7e98a237a03
glenn.syj <glenn.syj@gmail.com>
2024-07-29 11:01:54 +0900

    S11P12A807-225 fix: CORS 정책 및 Response 수정
    Former-commit-id: 5affc12badddfa14df6e502a40455c94ba9c028a

--- FE/code-arena/src/components/programlang/ProgramLangImg/ProgramLangImg.tsx
+++ FE/code-arena/src/components/programlang/ProgramLangImg/ProgramLangImg.tsx
@@ -1,3 +1,11 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:ac18a68ddc6f30d8a3a2dcb5ea7734dda4261ddb83c0e45542b58d019818e2b7
-size 231
+import React from "react";
+
+interface Text {
+  name?: string;
+}
+const ProgramLangImg: React.FC<Text> = (Text) => {
+  const imgSrc = "/images/" + Text.name + ".png";
+  return <img src={imgSrc} />;
+};
+
+export default ProgramLangImg;
