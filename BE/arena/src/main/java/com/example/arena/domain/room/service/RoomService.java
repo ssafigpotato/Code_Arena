5b71d37bee53f8639bbcce52bbe465940fb3ea5f
진주원 <drasgon@naver.com>
2024-07-29 16:02:41 +0900

    [FEAT : BE S11P12A807-185] Room Service
    Former-commit-id: 2ed4f9a17365bfea016192b67b2c2c53d47936ce

--- BE/arena/src/main/java/com/example/arena/domain/room/service/RoomService.java
+++ BE/arena/src/main/java/com/example/arena/domain/room/service/RoomService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:93982a98622d8d57d116346e5ac66282800dfa45b5c3f17d77ef71518a4d4b2b
-size 3537
+oid sha256:a9b485ebf54bb7c22db769e2f0d444988569287b937fb5a59a4799ba654bc087
+size 3786
