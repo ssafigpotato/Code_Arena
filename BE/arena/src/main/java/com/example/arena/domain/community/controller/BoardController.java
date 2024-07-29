c7952db271ee43f1a22af1647bc03a7f3653e7cc
허동민 <gjehdals456@gmail.com>
2024-07-29 11:16:01 +0900

    S11P12A807-224 fix : 게시물 상세보기 response comments 추가 - done : boardresponse에서 detailboardresponse로 변경
    Former-commit-id: c9c6bdba6f06aa5991489ac9c06b7171762c4ee3

--- BE/arena/src/main/java/com/example/arena/domain/community/controller/BoardController.java
+++ BE/arena/src/main/java/com/example/arena/domain/community/controller/BoardController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bb27c49ffc2cd0cc64cd4aeeea6c170f5c7b4d4afa0d7236f8a215df0473a412
-size 3959
+oid sha256:38b722038561bdf64df14db793d0127b5d907f311d90324e0d078ab768453e7b
+size 3965
