4db30177bedc3e0a49ad77747f574b983638f61d
glenn.syj <glenn.syj@gmail.com>
2024-07-29 11:01:54 +0900

    S11P12A807-225 fix: CORS 정책 및 Response 수정
    Former-commit-id: dcca353fd8ab52b68aa0d869ef925c51cc13d1e9

--- BE/arena/src/main/java/com/example/arena/domain/config/WebConfig.java
+++ BE/arena/src/main/java/com/example/arena/domain/config/WebConfig.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d3241cc479a82676d91828f0b7a897e02daef6b2e135290ce4ac43b381ccf50b
-size 856
+oid sha256:9136ea72dd9d10e21f762bf606601212953a13326d04feacb10deeb9744926ee
+size 858
