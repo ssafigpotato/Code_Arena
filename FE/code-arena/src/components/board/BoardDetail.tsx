a6098cb01b1faa0c6fda63382969562a98c8a488
ryucream612 <ryucream612@naver.com>
2024-07-29 14:14:02 +0900

    S11P12A807-141 feat: userstore 작성, 좋아요 코드 작성
    Former-commit-id: cd0f9b105f444b60cb9312ee870c0ec85c6b6658

--- FE/code-arena/src/components/board/BoardDetail.tsx
+++ FE/code-arena/src/components/board/BoardDetail.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7ce751d0aeaad413cf1cffc843b0ab4dd8773914bcb92838fb83a02e8ecf18bc
-size 8855
+oid sha256:da936a718635eebca14eb9785bc4763bb8e2224a70d23e79eece6a0dc7999924
+size 11385
