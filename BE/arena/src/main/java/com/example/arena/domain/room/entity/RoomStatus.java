ee5efffc332e7f0a301b0fb6d9d75784eb1edaad
glenn.syj <glenn.syj@gmail.com>
2024-07-29 10:38:29 +0900

    S11P12A807-222 fix: header 관련 webconfig 수정
    - done: 회원가입시에는 JWT Filter 적용하지 않는 코드 추가
    
    
    Former-commit-id: 20c3613201643c847f14a9f51d72409186f8b484

--- BE/arena/src/main/java/com/example/arena/domain/room/entity/RoomStatus.java
+++ BE/arena/src/main/java/com/example/arena/domain/room/entity/RoomStatus.java
@@ -1,3 +1,5 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:84f03b994b966c0210d1a8d8bc8145813701d401e22cc84652514b9d08940f7a
-size 94
+package com.example.arena.domain.room.entity;
+
+public enum RoomStatus {
+    PRIVATE, PUBLIC
+}
