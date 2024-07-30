3d0db5a6fa339e18c75885490fcbbf9d2033f4e7
Dohyungh <bley1217@gmail.com>
2024-07-30 17:53:21 +0900

    S11P12A807-177 feat: wss 연결확인
    Former-commit-id: b8f0df7ad59f84f0bcde89bd1d8f7f522e0e803a

--- BE/arena/src/main/java/com/example/arena/domain/config/WebSocketConfig.java
+++ BE/arena/src/main/java/com/example/arena/domain/config/WebSocketConfig.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7231f8c70d9909327fc7c774fb8e4f88a69e21d4b6c94712880b0b49cf4865bf
-size 818
+oid sha256:bc802f9d58955cc76ee459e4007be21f418e4f5af297641e9a604859f54db101
+size 843
--- BE/arena/src/main/java/com/example/arena/domain/kurento/handler/KurentoHandler.java
+++ BE/arena/src/main/java/com/example/arena/domain/kurento/handler/KurentoHandler.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ac90877d52c382c53d428dfd0e466d61cac2320ef351c7f31d0f4947b707b89f
-size 3620
+oid sha256:e904a60dff777956cb6cd658613bea3be91ccb9dc0f86099b70a7fd5555db9df
+size 3917
