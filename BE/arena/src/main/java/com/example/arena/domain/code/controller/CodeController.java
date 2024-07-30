7b2332da98c4647283565bee246b09522c362a58
허동민 <gjehdals456@gmail.com>
2024-07-30 16:07:57 +0900

    S11P12A807-224 feat : 실시간 코드 에디터 init - done : 실시간 스트리밍 가능 - need : roon별로 스트르밍할 수있도록 세팅 - need : security conifig에 /ws/** 추가할 수 있도록 설정 필요
    Former-commit-id: 35d13f094b9c2a05cc540933754443a1ba183ebb

--- BE/arena/src/main/java/com/example/arena/domain/code/controller/CodeController.java
+++ BE/arena/src/main/java/com/example/arena/domain/code/controller/CodeController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:82f6ab0f6d0e20898df98f765e53075de37d0db0c77e49e2ade572332f45a376
-size 1703
+oid sha256:32c0a38513623e8013526d4ab65acafc9eaece95259102f4db7ecb400176da52
+size 1696
--- BE/arena/src/main/java/com/example/arena/domain/config/WebConfig.java
+++ BE/arena/src/main/java/com/example/arena/domain/config/WebConfig.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fda8db3bb0175803c60b78f6f98804f88194373b09439632c1cc882a4ab01f59
-size 770
+oid sha256:3763a1d94529243eed332d07e715e2a90127eed98a6bd65eeb4450a356971a89
+size 704
