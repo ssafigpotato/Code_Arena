2a6c9013d15e4b7a2c0eb9846aa5d5e165981954
허동민 <gjehdals456@gmail.com>
2024-07-30 10:22:14 +0900

    S11P12A807-224 hotfix : 게시글 최신순으로 정렬 - done : findbyboardtype에서 findbyboardrtpypebycreatedad으로 수정
    Former-commit-id: a2fbf04cfbde881fcad7984c5b417f17d96e15b1

--- BE/arena/src/main/java/com/example/arena/domain/community/repository/BoardRepository.java
+++ BE/arena/src/main/java/com/example/arena/domain/community/repository/BoardRepository.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2c327825ef3cd348c12475ad5930fbcebaae6afdd1078a734cd7022c2388c84f
-size 1761
+oid sha256:7479775c0dd198fac6258154c4979655aa14637526133e5468ebfb207559f545
+size 1781
