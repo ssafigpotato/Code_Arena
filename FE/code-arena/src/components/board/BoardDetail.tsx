6e274fa6469f1e7ee8c2ba6aaa7fe8d675a6e082
ryucream612 <ryucream612@naver.com>
2024-07-30 10:43:25 +0900

    S11P12A807-144 feat: 댓글창 모달 작동하도록 추가
    Former-commit-id: 3c48eb431a2e2a479e2fc719456bccb96cbd3ff2

--- FE/code-arena/src/components/board/BoardDetail.tsx
+++ FE/code-arena/src/components/board/BoardDetail.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2a41ae7d59708e17098ace7da0a5f3c1d8cce44c25ffcd59df79d38bc925d8fb
-size 12143
+oid sha256:3a7e19036a4c3a9ce2d9a6434f15db911f51841e0be43faddfa4f30141929851
+size 13368
--- FE/code-arena/src/store/commentstore.tsx
+++ FE/code-arena/src/store/commentstore.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:406a42bacfb503449aee0873ba975a48a79e09da88fa63a48d9ea2ec18389d70
-size 1744
+oid sha256:c302d92c33504071fdcb991fe73a59969b8c438db1886dfb2fcada01fbba2af2
+size 1730
