d3e976a6e2873b1dd6bfd83c9b1c36d64412cd62
ryucream612 <ryucream612@naver.com>
2024-07-30 17:54:19 +0900

    S11P12A807-129 feat: 그룹 상세 조회 코드 작성 - need: 백엔드와의 연동 확인 - need: 주석 처리된 부분들 확인
    Former-commit-id: 268ce9520885cefb35a6c9fc840e99d0c7bee121

--- FE/code-arena/src/app/group/[id]/page.tsx
+++ FE/code-arena/src/app/group/[id]/page.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f335855070fa89f86648352e2c7641e31c0ab1d9aadbe8292878ad6d92d417b5
-size 17495
+oid sha256:5c46e72e904cbe7b0a5f330c928520ce329c11dc8b2bd3596ed3139c7d0bf547
+size 16564
