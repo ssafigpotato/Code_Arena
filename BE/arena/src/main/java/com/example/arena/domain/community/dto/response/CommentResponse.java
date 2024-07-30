b3f608f871c586c75927e6cbe4c414a9751e4544
허동민 <gjehdals456@gmail.com>
2024-07-30 12:51:16 +0900

    S11P12A807-224 feat : comment response commentid 칼럼 추가 - done : add commentid at commentreponse
    Former-commit-id: fabde3798e932cce3a3c99a0f3cd65657326727f

--- BE/arena/src/main/java/com/example/arena/domain/community/dto/response/CommentResponse.java
+++ BE/arena/src/main/java/com/example/arena/domain/community/dto/response/CommentResponse.java
@@ -1,15 +1,3 @@
-package com.example.arena.domain.community.dto.response;
-
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-
-@Getter
-@NoArgsConstructor
-@AllArgsConstructor
-public class CommentResponse {
-	private String content;
-	private int likes;
-	private boolean isSecret;
-	private String nickname;
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:d4b25f371cae445ad1dee38348c502f09aca44cc9f68ad55e5e608b2682e9664
+size 394
