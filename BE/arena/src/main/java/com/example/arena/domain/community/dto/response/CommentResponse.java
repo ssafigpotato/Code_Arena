a4d97c23d77727c7b3cf1c73d4ab7d81c33ad99f
허동민 <gjehdals456@gmail.com>
2024-07-30 12:51:16 +0900

    S11P12A807-224 feat : comment response commentid 칼럼 추가 - done : add commentid at commentreponse
    Former-commit-id: 3d48155023e504e60bfba26852664f63c6bfcd5b

--- BE/arena/src/main/java/com/example/arena/domain/community/dto/response/CommentResponse.java
+++ BE/arena/src/main/java/com/example/arena/domain/community/dto/response/CommentResponse.java
@@ -1,5 +1,7 @@
 package com.example.arena.domain.community.dto.response;
 
+import java.util.UUID;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -8,6 +10,8 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class CommentResponse {
+	// memberId 담아야함
+	private UUID id;
 	private String content;
 	private int likes;
 	private boolean isSecret;
--- BE/arena/src/main/java/com/example/arena/domain/community/mapper/CommentMapper.java
+++ BE/arena/src/main/java/com/example/arena/domain/community/mapper/CommentMapper.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ea03e4e9ca773a77080f46d9023d80049886315bd17b32ed0d1601bad1c75b84
-size 1130
+oid sha256:04a831a7f3b31065a3643b0716e244bd53e5b8a98fa82e1ba1dd57670c7eeba8
+size 1146
