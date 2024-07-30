2eb5a847a014e9f1e097adcca244d67a4511baf6
Dohyungh <bley1217@gmail.com>
2024-07-30 16:58:19 +0900

    S11P12A807-177 feat: kurento 연동파일 스켈레톤 코드 작성 완료
    Former-commit-id: 0167140e10291d362e0b29f7b466df19dc6cff79

--- FE/code-arena/conferenceroom.js
+++ FE/code-arena/conferenceroom.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8986f80ee467819a8497bcd725f88597379b66d3ed2056bc784b0d46810fa862
-size 4854
+oid sha256:d8fd4eaed4dda7aea5108bcbfdf40a7a8c32047b6f0f8b9d58fbbac087461296
+size 4929
