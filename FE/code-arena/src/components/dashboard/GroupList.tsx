5a52dc1f14d5877aa6e4d1a1163fd15b57f07c33
ryucream612 <ryucream612@naver.com>
2024-07-30 16:21:12 +0900

    S11P12A807-129 feat: 그룹 생성 코드 작성
    Former-commit-id: 4f6d2ce2727b4e31b3cf2efd26fb83c2b5200a1e

--- FE/code-arena/src/components/dashboard/GroupList.tsx
+++ FE/code-arena/src/components/dashboard/GroupList.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:39befbda7e944e341cc3536a7a8b57f835b0c54c00a22aa225bfca5140d5ffff
-size 14255
+oid sha256:b9c548174c4dba7c569cadae5f273e7b89eae03b0ba61faa4a836fc990908ed9
+size 17229
---
+++ FE/code-arena/src/store/groupstore.tsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:8990ae2b6375d78334f2a2bbff5136b132d82f5fba05f38b79d995d1c08e62d5
+size 1157
