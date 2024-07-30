119bacbc48876aff1a014d90c2665fcb7afd301a
허동민 <gjehdals456@gmail.com>
2024-07-30 10:46:44 +0900

    S11P12A807-224 fix : 댓글 entity에 baseenetity 추가 - done : add comment -> baseentity
    Former-commit-id: 2d5316ed4836118d7f1ef762311aa729e9c14486

--- BE/arena/src/main/java/com/example/arena/domain/community/entity/Comment.java
+++ BE/arena/src/main/java/com/example/arena/domain/community/entity/Comment.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d53f5b6fab71f66619687da6d5f5fca065992b617b4cffb4e75973015c6f55d0
-size 1142
+oid sha256:bb4cef4a88577aca561a8573c19a89f3b2d0bb192845b22670dfe3eb5c13e773
+size 1161
