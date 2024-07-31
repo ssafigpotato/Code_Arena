2b093770f9eeae80e5ecce319355f6cac34e2b5d
진주원 <drasgon@naver.com>
2024-07-31 11:17:30 +0900

    [FIX : BE S11P12A807-242] fix group logic
    Former-commit-id: bd9ba27f22b25da838e6006272455712794ae4af

--- BE/arena/src/main/java/com/example/arena/domain/group/entity/GroupMember.java
+++ BE/arena/src/main/java/com/example/arena/domain/group/entity/GroupMember.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:10643a96ea8dac39eebda1c927401954067996c04028a4235f092cfcfa815046
-size 1122
+oid sha256:56369fd0f1ba53032d19648c51bd94459d95bdaee86894fd7614b42bfd53426a
+size 1267
