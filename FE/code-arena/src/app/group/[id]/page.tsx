889c8d8842d5255ba945da978efaae6986b717ef
ryucream612 <ryucream612@naver.com>
2024-07-31 15:30:24 +0900

    S11P12A807-247 feat: 주석 추가, confirm 창 멘트 수정
    Former-commit-id: b4423f970203ebcbfed89d5addb7809105488b73

--- FE/code-arena/src/app/group/[id]/page.tsx
+++ FE/code-arena/src/app/group/[id]/page.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f2ce62db0010afa566a1fd99efeebc84a4bb72f800458f18db5b6731a8a4cc9c
-size 19139
+oid sha256:83309462e8eb7b2046189b64f9729dfd31132be6c573b7464a7d6a1378fb2284
+size 19165
