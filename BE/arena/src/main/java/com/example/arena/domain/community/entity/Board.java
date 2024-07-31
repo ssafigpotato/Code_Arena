dc68bb1deaeb9929fe1e99c4295e6dc3b16867fd
허동민 <gjehdals456@gmail.com>
2024-07-31 11:27:38 +0900

    S11P12A807-238 fix : baseentity 경로 변경 - done : basenetity 경로 board.entity에서 global.entity로 변경 - need : test 필요
    Former-commit-id: 3dc4327a2bf659bb6e3d4b63425b74218896fe57

--- BE/arena/src/main/java/com/example/arena/domain/community/entity/Board.java
+++ BE/arena/src/main/java/com/example/arena/domain/community/entity/Board.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6b5cda1c7b7570a5c0e3708fd84ae9970e30090f35d0cf1a55f77bee6fc775f6
-size 1797
+oid sha256:3c9a1fd2839ad4bd7bd0eb0fc0c65ea66e178569c11486f1306c52ce5288e0f7
+size 1849
