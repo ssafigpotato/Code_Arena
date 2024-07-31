c5cac39701829f76c240b72d3f70d3152b15facc
진주원 <drasgon@naver.com>
2024-07-31 11:17:30 +0900

    [FIX : BE S11P12A807-242] fix group logic
    Former-commit-id: a6cdd6c3e4f136747f27daed2138059dc183f22c

--- BE/arena/src/main/java/com/example/arena/domain/group/controller/GroupController.java
+++ BE/arena/src/main/java/com/example/arena/domain/group/controller/GroupController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:afcef3daa58ffb591c645e912d2f25497df460d64c74d2ee0d8a5c54c4af9573
-size 2673
+oid sha256:38f8a540237f6cc80abc3136e65afa1d104000b065134ad5fe3444e084a5a780
+size 2679
--- BE/arena/src/main/java/com/example/arena/domain/group/mapper/GroupMapper.java
+++ BE/arena/src/main/java/com/example/arena/domain/group/mapper/GroupMapper.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4d62b49444f919c58fecf1fa81f60c22b3506a5426a5f1daf257a34a7b4526a1
-size 2917
+oid sha256:b9f397e9df5d53d623942bd9515f8b2172865f6b9f1932b93b56f3e4008fbc8d
+size 3431
