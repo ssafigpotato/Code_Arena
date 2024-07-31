0955fca3e28711b132efb6cf4e9335b9d7273553
ryucream612 <ryucream612@naver.com>
2024-07-31 14:43:41 +0900

    S11P12A807-129 feat: 그룹 리스트 조회, 상세 조회, 생성 코드 작성 - need: createdAt 백엔드에서 넣어주면 렌더링 - need: 로그인 만료 시 테스트 - need: 누적 미팅 시간 렌더링 - need: 자기 자신에게 강퇴 버튼 보이지 않게 렌더링 - need: 리더 체크표시 렌더링
    Former-commit-id: 559d723104a4a1f98df4ea4e3fb11e5d4eec81d3

--- FE/code-arena/src/components/carousel/GroupMember.tsx
+++ FE/code-arena/src/components/carousel/GroupMember.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:453ae6fd24fc1213be9dc4a4f12a45d33b52404be3a2658cd1ccdcd179d10b7d
-size 3519
+oid sha256:3ba574172fc8517e2df5815c6ae6313267b2f927aa42bfe4df45753c14f09539
+size 3566
