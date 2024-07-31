8b88aab02e616ec166d15fcd59237c2070abe47c
bley1217 <bley1217@gmail.com>
2024-07-31 20:18:02 +0900

    Revert "Merge branch 'feat/BE/S11P12A807-249-sec-config' into 'master'"
    This reverts commit 1d7ee2c571983dd0cae4675bde1a579b23bd47ab [formerly b573bd152003c7afae85b7b2e0cdf80e6b5a5706], reversing
    changes made to 33e34f798a4b83787ab2248ca8a643c82f89bd3d [formerly 93f9b9c04a2835f96f5cb3d834a2a8a5da07bd71].
    
    
    Former-commit-id: 46cd56a595e6140c61007883686fb05621b48d27

--- BE/arena/src/main/java/com/example/arena/domain/config/WebConfig.java
+++ BE/arena/src/main/java/com/example/arena/domain/config/WebConfig.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:af7fd42be1eae22d363268d53bcc0fb9136f281324872f4e4343d9b91b697d50
-size 955
+oid sha256:20d0dda05f3fde408b3aaa089fa9d21dc3752c3d50ac5e9727a7e020c2c1908a
+size 882
