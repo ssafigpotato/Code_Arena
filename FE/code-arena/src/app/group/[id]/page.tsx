e56725d5be2597eec9bf6e1833c182ba3ae217ee
ryucream612 <ryucream612@naver.com>
2024-07-31 15:24:46 +0900

    S11P12A807-247 feat: 그룹 삭제 코드 작성 - need: 백엔드와의 연동 확인
    Former-commit-id: 120a23e789824dffe3b2d3994e86da803bf15759

--- FE/code-arena/src/app/group/[id]/page.tsx
+++ FE/code-arena/src/app/group/[id]/page.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fb057ccfb57caa132da16c216722aedd12d766bcf12eb563a4a379ad1da792ea
-size 18409
+oid sha256:f2ce62db0010afa566a1fd99efeebc84a4bb72f800458f18db5b6731a8a4cc9c
+size 19139
