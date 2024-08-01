250655b4cbdcb5553c87b337eb0d08d194751d5e
inception010203 <inception010203@gmail.com>
2024-08-01 11:39:11 +0900

    S11P12A807-254 feat: 코드 작성란 컴포넌트 생성
    Former-commit-id: 0a411f1a29ac4fb1b82cdbed50f85356e615faf8

--- FE/code-arena/src/components/room/code-component/CodeEditor.tsx
+++ FE/code-arena/src/components/room/code-component/CodeEditor.tsx
@@ -1,14 +1,3 @@
-import React, { useState, useRef, useEffect } from "react";
-import styled from "styled-components";
-import BorderButton from "@/components/common/button/BorderButton";
-import Link from "next/link";
-
-const Editor = () => {
-  return (
-    <>
-      <h1>코드 편집기</h1>
-    </>
-  );
-};
-
-export default Editor;
+version https://git-lfs.github.com/spec/v1
+oid sha256:1c9b401be75dcb4ed0ec916e1215f1ab1f726665a5d6569cdf946bfdb3e1f702
+size 693
