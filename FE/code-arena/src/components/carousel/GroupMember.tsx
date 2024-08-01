ec8287c7a63040af6d0ae251f7cf610dc91f71b0
ryucream612 <ryucream612@naver.com>
2024-08-01 17:18:39 +0900

    S11P12A807-130 feat: 그룹 가입 대기중 상태 추가, 가입된 멤버만 렌더링, 리더 체크 표시 렌더링
    Former-commit-id: b391fc86c27b0ed7ccfa009ac38a4784ecd9d7c0

--- FE/code-arena/src/components/carousel/GroupMember.tsx
+++ FE/code-arena/src/components/carousel/GroupMember.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3ba574172fc8517e2df5815c6ae6313267b2f927aa42bfe4df45753c14f09539
-size 3566
+oid sha256:9d164bb23d3369e715d0dee7f9a6a6e191f2ada9964e37cb9b516e1aab962c3d
+size 3567
