a44df16118bd06ac02b2f923d96de46bbd8afc4d
ryucream612 <ryucream612@naver.com>
2024-08-01 17:56:36 +0900

    S11P12A807-267 feat: 그룹 정보 수정란에 취소 버튼 추가 - need: 수정 로직 작성
    Former-commit-id: ff2f94afcf4a8096fd7ab5aae93bf0c4b570ffb7

--- FE/code-arena/src/app/group/[id]/page.tsx
+++ FE/code-arena/src/app/group/[id]/page.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:93dbd3bfb1bf11d141499ccfa4c965cedbece2d327e72d0c723ccb86a19b1540
-size 20798
+oid sha256:2f2478d29a54c4f1ad844b7aaaf454249faf9a2f3dcdc12d2e93a45120eb964d
+size 21216
