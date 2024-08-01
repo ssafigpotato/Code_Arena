6c7c9f5cd0061cc61b34654cae2be4832530e3f8
ryucream612 <ryucream612@naver.com>
2024-08-01 17:18:39 +0900

    S11P12A807-130 feat: 그룹 가입 대기중 상태 추가, 가입된 멤버만 렌더링, 리더 체크 표시 렌더링
    Former-commit-id: 9b9735cc20ecdb0794e08d7c4189a7401af836cc

--- FE/code-arena/src/app/group/[id]/page.tsx
+++ FE/code-arena/src/app/group/[id]/page.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e49bed6084c26c30238946b00f0ed7848b6e35094813d9cfc6c18a03fddab8fb
-size 19918
+oid sha256:40ef35ac7ca76d1f4d781db6aee3588597d34bb861c3f14c4a4938532056b586
+size 20751
--- FE/code-arena/src/store/groupstore.tsx
+++ FE/code-arena/src/store/groupstore.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:634cefb5296062d901800f94dd646a7b990f70a8eadfe156be06030bfd5b20aa
-size 5219
+oid sha256:25844f6acfc6c61d60690f0267d4913dffd2a72ff76ca3291d4cac43562c1e31
+size 5224
