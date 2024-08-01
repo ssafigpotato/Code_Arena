e831cf9e0e280193f33488314154195e3d078a71
glenn.syj <glenn.syj@gmail.com>
2024-08-01 12:51:43 +0900

    S11P12A807-252 feat: 프론트 이용을 위한 ErrorResponse 기반 에러 반환 코드 작성
    - done: ErrorResponse 및 ErrorType Enum 작성
    
    
    Former-commit-id: 0e7974fd24159e6f835efe1a0166e6fc33cb4a18

--- BE/arena/src/main/java/com/example/arena/domain/member/jwt/JWTFilter.java
+++ BE/arena/src/main/java/com/example/arena/domain/member/jwt/JWTFilter.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c55e280e8add4c65a482be2547d07510be89981072c8bcd25d306ac2af57e90d
-size 4441
+oid sha256:21e3abe036156ec43e3cae6f69002653011e49d64f8d8ae89a53465c0ed0cbf0
+size 5059
