47a49baf1551bff52d5b7f94e47c6fc6df59c195
ryucream612 <ryucream612@naver.com>
2024-08-01 12:51:19 +0900

    S11P12A807-215 feat: 게시글 수정 요청 헤더에 토큰 추가 - need: router.push의 경로 중 type이 뜨지 않는 오류 확인
    Former-commit-id: b25f74c728b14cde1fd94dd4f6874a621ed75bf5

--- FE/code-arena/src/components/board/BoardDetail.tsx
+++ FE/code-arena/src/components/board/BoardDetail.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1758a931027fd2a0c94c6487d262a124a3c4d90f1a3613c3e7fecc5a7f082137
-size 18102
+oid sha256:e8bf75d62b330af0cc9aa8a9fa212d9928a05ebd14e2472bb179ee61662844aa
+size 17951
