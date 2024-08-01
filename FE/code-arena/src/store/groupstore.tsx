09fb17388abbbc1145f4a27242635d0ade41bbe7
ryucream612 <ryucream612@naver.com>
2024-08-01 15:39:21 +0900

    S11P12A807-130 feat: 그룹에서 searchUser 사용하지 않도록 변경
    Former-commit-id: 4a4ba80ee374916e5cc72168f4aef5b4fa65cf80

--- FE/code-arena/src/store/groupstore.tsx
+++ FE/code-arena/src/store/groupstore.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3592de036b81bcf8c8ef0c0b3654d302d9b1ada2bd247157459e3552d5e78d83
-size 4248
+oid sha256:634cefb5296062d901800f94dd646a7b990f70a8eadfe156be06030bfd5b20aa
+size 5219
