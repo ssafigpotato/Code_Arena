73877758b156db2916cfd217c5c952c8d3e33440
진주원 <drasgon@naver.com>
2024-08-01 17:16:58 +0900

    [HOTFIX : BE S11P12A807] Group Hot Fix
    Former-commit-id: 5e9a00c3f255dd578e57c5f5b802e631e623a2fe

--- BE/arena/src/main/java/com/example/arena/domain/group/dto/response/GroupResponse.java
+++ BE/arena/src/main/java/com/example/arena/domain/group/dto/response/GroupResponse.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6e8277e159736fdbda6af7585875dbab709e333c1cb95490b8dfec65fb7a4e37
-size 661
+oid sha256:67a07941d832d2b957951b503ccd1b990c20ea10378f26d071821689c1a2faf7
+size 767
