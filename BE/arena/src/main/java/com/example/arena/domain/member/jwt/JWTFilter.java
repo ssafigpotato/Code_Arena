60ff5877fdfcf00c3f16daccbfd8569124650f1c
glenn.syj <glenn.syj@gmail.com>
2024-08-01 12:59:30 +0900

    S11P12A807-252 fix: 액세스 토큰 만료 시간 변경 및 에러 응답 헤더 추가
    - done: Arena-Error-Code 헤더 이용 가능
    - done: 액세스 토큰 시간 30분으로 수정
    
    
    Former-commit-id: 8a46abd89aebe455682fee3b692029c2afdeabba

--- BE/arena/src/main/java/com/example/arena/domain/member/jwt/JWTFilter.java
+++ BE/arena/src/main/java/com/example/arena/domain/member/jwt/JWTFilter.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:21e3abe036156ec43e3cae6f69002653011e49d64f8d8ae89a53465c0ed0cbf0
-size 5059
+oid sha256:4aac87e89d50464fa41ef99a78b8b83228fbda6a2afe80dcf11ea3575f381cf7
+size 5125
