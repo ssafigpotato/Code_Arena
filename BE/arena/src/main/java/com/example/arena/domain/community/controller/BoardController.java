887e47572dda5768d94d7bbf68f4653a218d7a68
glenn.syj <glenn.syj@gmail.com>
2024-08-02 10:01:45 +0900

    S11P12A807-230 style: post 변수명 직관적인 board로 수정
    Former-commit-id: 66e30c15b5277e1640585f85be7aa0b4fef5a466

--- BE/arena/src/main/java/com/example/arena/domain/community/controller/BoardController.java
+++ BE/arena/src/main/java/com/example/arena/domain/community/controller/BoardController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:38a64d0f974f3103b0187adcd3d3de917c3ad5515949b1642eb9b769dc8ec39a
-size 4459
+oid sha256:32804e22c89d9fa73250912d31c6bf268a04c1db3485e218cea7dd7ee36b3f37
+size 4461
--- BE/arena/src/main/java/com/example/arena/domain/community/service/BoardService.java
+++ BE/arena/src/main/java/com/example/arena/domain/community/service/BoardService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:88e9ff74474c67793aacb01a8bf2758e2e934986bef0a0399ad03ac1d9a1f389
-size 8150
+oid sha256:3286a71d466c3b53f9b4a241230fb0dd9f6cd4139505e3dc1d517d225f110e78
+size 8136
