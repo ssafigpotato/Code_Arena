55835f60ac62a0221d11c79a29c5c45c05e93101
inception010203 <inception010203@gmail.com>
2024-08-02 09:55:36 +0900

    S11P12A807-248 fix: 헤더에서 searchResult 관련 로직을 삭제하고, 사용자 임의 로그아웃시에도 accessToken이 null이 되도록 설정
    Former-commit-id: b4ec5a60243c97ae9acde9339f4038ab5cdddcfa

--- FE/code-arena/src/components/common/header/header.tsx
+++ FE/code-arena/src/components/common/header/header.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6ff2357d1eba5d73183714110b82a39897f134233dc89a23e3faa27dbd7a5f9b
-size 9771
+oid sha256:541f14a5a4eb2c3105edb8521a8fe1812481d509c4e66888b11a5c421a5dd66c
+size 9810
