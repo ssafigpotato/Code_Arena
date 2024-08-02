b1985b36a02cf193158d0cbe99716f6efd5703ed
진주원 <drasgon@naver.com>
2024-08-02 14:45:54 +0900

    [HOTFIX : BE S11P12A807] PUT Request Body FIX
    Former-commit-id: 13646c732c6d73823d8a781295c4032088b1af08

--- BE/arena/src/main/java/com/example/arena/domain/group/service/GroupService.java
+++ BE/arena/src/main/java/com/example/arena/domain/group/service/GroupService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:537426103ef25ff63747d430356e57b61eaa8ba571da73518bb22cc768eb8d38
-size 6214
+oid sha256:cd2e3e1895f91f8d71bfd32922738019486d570b15c2737e1a3c4ecf1a77d17a
+size 6305
