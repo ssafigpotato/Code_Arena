7ddefd8ead0f532c3de3a0dbaf32c77397509a4c
glenn.syj <glenn.syj@gmail.com>
2024-08-02 17:51:29 +0900

    S11P12A807-269 test: 컨트롤러 테스트 로그인 및 회원가입 통과
    - done: security config 내 proivder 설정 변경
    
    
    Former-commit-id: 1edaba5d4d4266145b29fc46bebc1796b97872c8

--- BE/arena/src/main/java/com/example/arena/domain/config/SecurityConfig.java
+++ BE/arena/src/main/java/com/example/arena/domain/config/SecurityConfig.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:93ddd3fde2f9f3190672b5a67ff2238b07b2fb335aa9e1011344accbca33d141
-size 5986
+oid sha256:83c2423f840e3ea7b51cf7f8418434af176a6c6ad2ee191d15b85874ff7dfa25
+size 6441
