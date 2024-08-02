cd9122230c86acddbe8a7bc31eb14c9ca66cdf71
ryucream612 <ryucream612@naver.com>
2024-08-02 09:50:59 +0900

    S11P12A807-267 feat: 컴포넌트에서 그룹 가입 수락, 거절 로직 구현 - need: 백엔드와의 연동 확인 필요
    Former-commit-id: 3a905d1d82e73d64eaeb8f21e2c91909cf26e264

--- FE/code-arena/src/app/group/[id]/page.tsx
+++ FE/code-arena/src/app/group/[id]/page.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dd9bbfc5383e21d51f2616b8ca15ab1079e09d43e6aac8cb36fb1fb5f0ba43ea
-size 21252
+oid sha256:8d474b62c77d5c9a41e11e2db42348e5a4283ed57221303540ee07f16f67d4b2
+size 22590
