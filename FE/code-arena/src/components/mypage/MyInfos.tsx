16400154e71830150df823f300c9e146f360f5e6
inception010203 <inception010203@gmail.com>
2024-08-05 10:51:09 +0900

    S11P12A807-280 fix: 그룹카드에서 그룹 제목, 그룹 리더의 이름이 너무 길 때 div를 넘어가지 않도록 수정
    Former-commit-id: df8fa405aaa08e04e3d3a92db9be4e41b1e1822e

--- FE/code-arena/src/components/mypage/MyInfos.tsx
+++ FE/code-arena/src/components/mypage/MyInfos.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:da47be4d08683521b97da70e365ffc4bcc80154ba15f4d28b2944467fc196a5d
-size 11553
+oid sha256:43620eaf3d1517ca30f2b2bb699c898e2bd52665e8953c5b88e53b5c9d3a1b48
+size 12021
