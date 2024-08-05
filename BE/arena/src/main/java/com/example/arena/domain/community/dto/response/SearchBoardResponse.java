9ce3b2398ac3e7a80047a84d723603aa0e9770c9
허동민 <gjehdals456@gmail.com>
2024-08-05 16:16:27 +0900

    S11P12A807-295 fix : search response body 추가 - need : test 필요
    Former-commit-id: 5d6d695046c01600e97be85653f65e9516b6816d

--- BE/arena/src/main/java/com/example/arena/domain/community/dto/response/SearchBoardResponse.java
+++ BE/arena/src/main/java/com/example/arena/domain/community/dto/response/SearchBoardResponse.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8bbf75718a67f424bdb178ff0d4339ef98b1977c0efabef5c2f1099b94d2af56
-size 579
+oid sha256:12c24af20104c3917f5b23d6272f4673486e2e7f200a12257374899f1b7354fe
+size 625
