5d2a12aadeab1470c10e6a0d4cdf089b59206464
inception010203 <inception010203@gmail.com>
2024-08-05 10:08:43 +0900

    S11P12A807-280 fix: 로그인한 유저가 아닐 때, 사용 찾을 수 없습니다가 뜨도록 수정
    Former-commit-id: 4487a908b901d59ecad46c332703bcb18e2b09da

--- FE/code-arena/src/components/mypage/MyInfos.tsx
+++ FE/code-arena/src/components/mypage/MyInfos.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:11b0bc8b20369223e55598469ca71b1d47da8a3bdf47d0d8cf20bbff75fae879
-size 10917
+oid sha256:88ef62755d53c51c1f3c0521e564ef9d90b065ca0281a58288396a78018c5457
+size 10924
