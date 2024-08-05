ec2de22a43e1360f12229cff0a489c871d5ceff0
inception010203 <inception010203@gmail.com>
2024-08-05 10:05:54 +0900

    S11P12A807-280 fix: 로그인한 유저가 속한 그룹 불러오기(더미데이터에서 실제 데이터로 수정)
    Former-commit-id: 7342f32412605520f686b8599e942b7d0f894007

--- FE/code-arena/src/components/mypage/MyInfos.tsx
+++ FE/code-arena/src/components/mypage/MyInfos.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d6967e37731b9b1be3d88abb4ab4d4aa9cfee5f768c1a37bdcdc56e5306d2996
-size 10014
+oid sha256:11b0bc8b20369223e55598469ca71b1d47da8a3bdf47d0d8cf20bbff75fae879
+size 10917
