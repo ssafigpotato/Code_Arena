9766f1eeef1f614a4d8a566aba9c3ffb986bb658
glenn.syj <glenn.syj@gmail.com>
2024-08-05 16:45:56 +0900

    S11P12A807-306 feat: 프론트-백엔드 연동 코드 추가
    - done: 프론트 openAI 이용 후 백엔드에 전달하는 코드 작성
    - done; 백엔드 코드 수정 및 관련 자료구조 UUID를 String으로 변경
    
    
    Former-commit-id: 9550f6be71a7ff5dd96eb96a19e0c041cbd88a50

--- BE/arena/src/main/java/com/example/arena/domain/code/controller/CodeController.java
+++ BE/arena/src/main/java/com/example/arena/domain/code/controller/CodeController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:512f82170c5430c130993ded9747c8dbfc9c27f046bd575daf2b75af0321fd12
-size 2089
+oid sha256:08dd6962a8b23ca82c35fbeb7c1b58e8f44b87dd15ce185d719b212b1dd863de
+size 2135
--- BE/arena/src/main/java/com/example/arena/domain/code/dto/request/TestCaseRequest.java
+++ BE/arena/src/main/java/com/example/arena/domain/code/dto/request/TestCaseRequest.java
@@ -1,24 +1,3 @@
-package com.example.arena.domain.code.dto.request;
-
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-
-import java.util.UUID;
-
-@Getter
-@NoArgsConstructor
-@AllArgsConstructor
-public class TestCaseRequest {
-    private UUID roomId;
-    private TestCase[] testcases;
-}
-
-@Getter
-@NoArgsConstructor
-@AllArgsConstructor
-class TestCase {
-    private int id;
-    private String in;
-    private String out;
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:d6a93804fb664d60323f57e7c974e9644e8d55922035f254cea661fcfdd10a36
+size 440
