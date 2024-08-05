def2540ac570954f678a55cd54d62da080d45e30
진주원 <drasgon@naver.com>
2024-08-05 10:45:23 +0900

    [FEAT : BE S11P12A807-300] Group Entity 속성 추가
    Former-commit-id: 6366174d81177091d8d7bb4d0ade26aaeef62b59

--- BE/arena/src/main/java/com/example/arena/domain/room/entity/Room.java
+++ BE/arena/src/main/java/com/example/arena/domain/room/entity/Room.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1a4dff2518230443b681a49535e1fd0310c912fbe4d3186778ec35b7568807bc
-size 1100
+oid sha256:10f26afbe08363146924fa2a7038e57bfeba7a9c216ffbf8da8e55139bc0c283
+size 1370
---
+++ BE/arena/src/main/java/com/example/arena/domain/room/entity/RoomLanguage.java
@@ -0,0 +1,5 @@
+package com.example.arena.domain.room.entity;
+
+public enum RoomLanguage {
+    Java, Python, CPP
+}
