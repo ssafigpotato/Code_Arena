5f7882dad4a1f2851fed698c02f59d6c243cb8ea
진주원 <drasgon@naver.com>
2024-08-05 11:10:31 +0900

    [FEAT : BE S11P12A807] Group Leader 로직 수정
    Former-commit-id: c27e3cac31a4558444cd4724937bc0cb5f79846f

--- BE/arena/src/main/java/com/example/arena/domain/group/entity/GroupMember.java
+++ BE/arena/src/main/java/com/example/arena/domain/group/entity/GroupMember.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:894ccf6fea9dd3169b52541d74eb4cfff43369ad8dd2e805479ff9d1c574e403
-size 1772
+oid sha256:67a66af2cc165126f9cf9cf671107c2e13e6c7dd415269afa19f6378dcb7dee8
+size 1880
