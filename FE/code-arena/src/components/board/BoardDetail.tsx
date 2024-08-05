7c19d3ed48625ba68c3020731d10ad838f5f3914
ryucream612 <ryucream612@naver.com>
2024-08-05 14:19:10 +0900

    S11P12A807-303 fix: 커뮤니티, 그룹에서 존재하지 않는 페이지 하드코딩
    Former-commit-id: 4200509db385fb211e660d71b131b280da619101

--- FE/code-arena/src/components/board/BoardDetail.tsx
+++ FE/code-arena/src/components/board/BoardDetail.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:374d8931476ee49b162906113edefe3030371875d420d8fc01e6cd489a1c6cd4
-size 18338
+oid sha256:8b05bac118fa38516b464a413d0692e229013f3d5d7e3883d1b874ad4bb1bc43
+size 19326
