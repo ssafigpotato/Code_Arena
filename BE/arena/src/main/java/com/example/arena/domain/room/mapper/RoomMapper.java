1aaa2bc6d0546ae351a78b32cc60871eb0da35bd
진주원 <drasgon@naver.com>
2024-08-05 10:50:14 +0900

    [FEAT : BE S11P12A807-300] Room Entity 속성 추가
    Former-commit-id: 67aa3b4dd0fdd0b7410ad43c556bbea9b4215d58

--- BE/arena/src/main/java/com/example/arena/domain/room/mapper/RoomMapper.java
+++ BE/arena/src/main/java/com/example/arena/domain/room/mapper/RoomMapper.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:531eae9d033ebd73c923ff198c23373fa80ebb5334df22beea37cc1ae66c8182
-size 2464
+oid sha256:e5e6c8ccbf1e1e971522af394f61d79cc00f200774285cc598be035185205137
+size 2463
