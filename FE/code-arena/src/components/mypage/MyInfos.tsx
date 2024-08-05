8115f12c231d5b5ef345b7fb2ac45db6064b750d
inception010203 <inception010203@gmail.com>
2024-08-05 10:44:17 +0900

    S11P12A807-280 fix: 그룹 카드에서 특정 그룹을 클릭시 상세페이지로 가도록 수정(더미데이터에서 실제 데이터로)
    Former-commit-id: 3d4912c9d047a7a2c6beff315cf8b2d293a85427

--- FE/code-arena/src/components/mypage/MyInfos.tsx
+++ FE/code-arena/src/components/mypage/MyInfos.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3d96af855225f32f47deabbbd7af42bd29dd9377d3d772df0570088f375e7ec8
-size 11472
+oid sha256:da47be4d08683521b97da70e365ffc4bcc80154ba15f4d28b2944467fc196a5d
+size 11553
