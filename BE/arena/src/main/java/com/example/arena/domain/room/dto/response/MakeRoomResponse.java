979bad230b976e7689e85888e51e11b2b4344248
진주원 <drasgon@naver.com>
2024-08-06 09:49:37 +0900

    [FIX : BE S11P12A807-312] Fix Room Logic
    Former-commit-id: 3ebbece654ca95805d7c502b9e27ad636f9e345d

--- BE/arena/src/main/java/com/example/arena/domain/room/dto/response/MakeRoomResponse.java
+++ BE/arena/src/main/java/com/example/arena/domain/room/dto/response/MakeRoomResponse.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:efade7bc2e76441aac1f890ea24cc856df7c0b2c4a474247bd64c27c84680d4b
-size 600
+oid sha256:06ac79a5c6ab695e96f2eb5cb6e3349a0b6faba322223720d145f34f8c92e0ec
+size 612
