16dbb4cefac7cca7a24bd237bae472748949daec
ryucream612 <ryucream612@naver.com>
2024-08-06 14:28:11 +0900

    fix: SearchBar의 onSearch를 선택적으로 받을 수 있도록 수정
    Former-commit-id: 5970c7394f371a371be9ef22312f618fbd6c2957

--- FE/code-arena/src/components/common/input/SearchBar.tsx
+++ FE/code-arena/src/components/common/input/SearchBar.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e36972b764bf9228b29050222d8eb1ca0f6770a31f37e23ee279e499ce2816c6
-size 1413
+oid sha256:0ad2164cb0c138009c57702a25648797a697aab30a4db3ac852fc28caeb272ac
+size 1442
