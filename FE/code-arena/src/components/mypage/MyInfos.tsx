25d0f907881fc683fa043501e30cb74e6fb254b1
ryucream612 <ryucream612@naver.com>
2024-08-06 10:13:22 +0900

    S11P12A807-313 feat: 내 그룹 불러오기 스토어 수정 - done: 더미 데이터 삭제 - done: api 요청 시 헤더에 토큰을 담아 요청하도록 수정 - done: 그룹 목록을  현재 사용자 이름으로 필터링하는 부분 불필요하여 삭제
    Former-commit-id: 50452d65cfb9174629b2c1ccf9c3e4922e896c0b

--- FE/code-arena/src/components/mypage/MyInfos.tsx
+++ FE/code-arena/src/components/mypage/MyInfos.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:43620eaf3d1517ca30f2b2bb699c898e2bd52665e8953c5b88e53b5c9d3a1b48
-size 12021
+oid sha256:6bdf8111426a1ffff4c7a8cc5f9f42c4c420f8d7cf3cb8e9011d8be0430072eb
+size 8673
