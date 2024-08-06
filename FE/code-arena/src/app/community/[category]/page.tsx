7ccab38bd71293aa315aca38608de34f8ee9d1ce
ryucream612 <ryucream612@naver.com>
2024-08-06 13:21:37 +0900

    S11P12A807-310 feat: 검색 엔터를 눌러야만 검색 목록이 렌더링되도록 변경
    Former-commit-id: af135f9600f7f67d1b12ecdcaaf9b86740495310

--- FE/code-arena/src/app/community/[category]/page.tsx
+++ FE/code-arena/src/app/community/[category]/page.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3f385dd4caf8d63325d46cfafc82e24c20c2c57b0fa230701f7a583814962fcf
-size 6089
+oid sha256:16b90d7d8d97ed17deb504885158d390e2ab8e70adfe881e519e1c5aa720161b
+size 6217
