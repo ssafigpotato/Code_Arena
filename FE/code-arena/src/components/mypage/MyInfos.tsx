6a5953f1f1ec56ecf5e83eab8a4c5657eb14b3c7
ryucream612 <ryucream612@naver.com>
2024-08-06 10:14:45 +0900

    S11P12A807-313 feat: 회원 정보 수정 클릭 라우터 링크 적용
    Former-commit-id: 7a88ac44b8a594dae8b49ed1a5a307ab65b5a1cb

--- FE/code-arena/src/components/mypage/MyInfos.tsx
+++ FE/code-arena/src/components/mypage/MyInfos.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6bdf8111426a1ffff4c7a8cc5f9f42c4c420f8d7cf3cb8e9011d8be0430072eb
-size 8673
+oid sha256:dcffad8e1358a837b0ce02af84120c03be856de0cbe4306bad91c1f9fdde4c1b
+size 8684
