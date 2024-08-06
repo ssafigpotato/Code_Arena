19961335e7d620fd5f00cb83adb4f9809d4cf42b
Dohyungh <bley1217@gmail.com>
2024-08-06 17:54:20 +0900

    S11P12A807-319 feat: room 입장 로직 작성중
    Former-commit-id: d821b80085481a0d271aaa985f6fd897c9c9aa70

--- FE/code-arena/src/components/room/roominfo-component/Timer.tsx
+++ FE/code-arena/src/components/room/roominfo-component/Timer.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8827e730d5ff971637e138960b29ba5130676ca84aab94cd82a37eb0761a7a0f
-size 2787
+oid sha256:750fa1281df68bf8e7859930c76af1a0c92f32c9b0c0a804157338bcf7580a4d
+size 2945
