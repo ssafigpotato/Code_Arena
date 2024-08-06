4e5a4c083c5d3eb0ebc3e4682a922a2894eb6a5f
ryucream612 <ryucream612@naver.com>
2024-08-06 17:02:10 +0900

    S11P12A807-317 feat: 닉네임 변경 로직 작성 - need: 이미지 변경, 비밀번호 변경 필요
    Former-commit-id: 260c9bd50a448235e9964af3f4357649faa3180d

--- FE/code-arena/src/app/mypage/edit/page.tsx
+++ FE/code-arena/src/app/mypage/edit/page.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b2452b4899beb41b87adfb576eab7cf202fee78a5e98aa7b5815d6f5c42ccc5a
-size 8835
+oid sha256:40e9952e5b6b91bacd9fbe771d2abae3886d30fb8e15efbd3a40706f356987be
+size 9331
