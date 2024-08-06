baa1b7c196a5e9b9bf86731acd90940cc72c0aca
Dohyungh <bley1217@gmail.com>
2024-08-06 17:54:20 +0900

    S11P12A807-319 feat: room 입장 로직 작성중
    Former-commit-id: c4c36364d2c141b3090f10b34650a4d8a9ea7c5e

--- FE/code-arena/src/components/room/RoomInfo.tsx
+++ FE/code-arena/src/components/room/RoomInfo.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cf9a44154c1f09ae0d5382172c8bdee99d27c36bc2ad4df4a41de972d008ef83
-size 7534
+oid sha256:106dfbd2588c336446b132c3c3617ab11cdaf751fa70359ab06a9f28853a1078
+size 7760
