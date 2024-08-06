a7172519a8428f82197d3612140a549b91e4c24c
허동민 <happycodingmin@gmail.com>
2024-08-06 15:16:40 +0900

    Update CodeEditor.tsx
    Former-commit-id: d6082458af10e6a1745013ede2bd74bbd5098745

--- FE/code-arena/src/components/room/code-component/CodeEditor.tsx
+++ FE/code-arena/src/components/room/code-component/CodeEditor.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:87e1ba792763f43bbbc12b245ce7efe9f05e146dc66917218339bdb9300ff844
-size 7000
+oid sha256:3d733ff28a1a74c4edadf843a4b77ed8148f6a286be08d97c4ec8e664b9e4e6d
+size 7244
