2da51bd0d68dc9468ff0c0be43bdc3ec9703d294
ryucream612 <ryucream612@naver.com>
2024-08-07 14:35:18 +0900

    S11P12A807-329 feat: 회원정보수정 페이지 닉네임에 정규식 적용
    Former-commit-id: 2eb8566d922992638bbd17ce1a088dc19fa21811

--- FE/code-arena/src/app/mypage/edit/page.tsx
+++ FE/code-arena/src/app/mypage/edit/page.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:40e9952e5b6b91bacd9fbe771d2abae3886d30fb8e15efbd3a40706f356987be
-size 9331
+oid sha256:4985ae3029107428169b4e0a79c6c0b630d544718cc666996967fcc3263d4d1d
+size 9814
