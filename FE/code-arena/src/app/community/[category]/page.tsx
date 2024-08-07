7f92c89db86a2e037096e8b9ebb1ceaf62e5ce14
ryucream612 <ryucream612@naver.com>
2024-08-07 16:10:37 +0900

    S11P12A807-332 feat: 백과 데이터 형식 일치시킴
    Former-commit-id: 1ea37e36581f90a1e15217662c9bdbac9068bacc

--- FE/code-arena/src/app/community/[category]/page.tsx
+++ FE/code-arena/src/app/community/[category]/page.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:543f0e50dec6f89cefee4474c96e2e8d6bcba6088c747763ae33dc1efc4e026c
-size 6947
+oid sha256:936b3ae7ceb83ba98b75d0031c1d6f1655006f1b542853f4dec0f98f23e43126
+size 6966
