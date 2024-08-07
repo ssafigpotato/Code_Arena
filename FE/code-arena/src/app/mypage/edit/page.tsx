223f8c8cd303066ccfa54696076a60d7e0b3a147
ryucream612 <ryucream612@naver.com>
2024-08-07 14:51:11 +0900

    fix: 이메일 명칭 통일
    Former-commit-id: 0d604f6cfd2577d781c65dd7bf55ac9aaa270d8e

--- FE/code-arena/src/app/mypage/edit/page.tsx
+++ FE/code-arena/src/app/mypage/edit/page.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4985ae3029107428169b4e0a79c6c0b630d544718cc666996967fcc3263d4d1d
-size 9814
+oid sha256:07bef0a0409ab29e5cb8ff5002795f1f4c4c59235c046743c317cd36dbfe715b
+size 9818
