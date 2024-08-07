cff7c6cd890410fdde32315cb1be021bd2b311c5
Dohyungh <bley1217@gmail.com>
2024-08-07 12:59:34 +0900

    S11P12A807-328 feat: room 입/퇴장 기능 완성
    Former-commit-id: 51b831f81ad4a663eb4a77a441836fdc2d22d4da

--- FE/code-arena/src/app/room/[id]/page.tsx
+++ FE/code-arena/src/app/room/[id]/page.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:064be126762d5edd80e61d8d7977e5f6ffe5ebeb8f7dc902c3c280d31a2fe370
-size 2513
+oid sha256:d09ddbeb07af733d61c13fd68e2be1636e9b80e5c775e254451ec433a73952ed
+size 2367
