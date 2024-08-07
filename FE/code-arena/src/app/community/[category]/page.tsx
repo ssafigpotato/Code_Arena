6c158c60b43c632d8c106c86dbb8e2612d280d1e
ryucream612 <ryucream612@naver.com>
2024-08-07 16:08:35 +0900

    S11P12A807-332 feat: 게시판 정렬 로직 작성, 추천순 주석처리
    Former-commit-id: e6f5c5f05aa69843d2248a27a2428a0f881d948c

--- FE/code-arena/src/app/community/[category]/page.tsx
+++ FE/code-arena/src/app/community/[category]/page.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:16b90d7d8d97ed17deb504885158d390e2ab8e70adfe881e519e1c5aa720161b
-size 6217
+oid sha256:543f0e50dec6f89cefee4474c96e2e8d6bcba6088c747763ae33dc1efc4e026c
+size 6947
