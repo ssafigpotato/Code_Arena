d706b9a4ccca5de2ca5adacad91d2441896e0a6a
Dohyungh <bley1217@gmail.com>
2024-08-08 12:47:09 +0900

    S11P12A807-340 feat: 룸 면접관과 응시자 간 ready/start 로직 작성중
    Former-commit-id: 146f932f1c685ef6dd95f8d4fde47cb94d18b4af

--- FE/code-arena/src/components/room/RoomInfo.tsx
+++ FE/code-arena/src/components/room/RoomInfo.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:08bae8ba7f5ebe17a6c9c58b5748394be1cc4c6a128fd9a8b569650250dcf92d
-size 7919
+oid sha256:118238da959254f80383bee579dcde06f6c4c6558fee12b79857f733d08e0457
+size 9006
