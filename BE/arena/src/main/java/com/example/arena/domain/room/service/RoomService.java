eee1db0087557e554271c1c6c5f02b5a2c4c24d2
진주원 <drasgon@naver.com>
2024-08-08 09:39:37 +0900

    [FEAT : BE S11P12A807-334] Room Ready
    Former-commit-id: 26ceb1274e80c69952890a288961edbd0b70c8ce

--- BE/arena/src/main/java/com/example/arena/domain/room/service/RoomService.java
+++ BE/arena/src/main/java/com/example/arena/domain/room/service/RoomService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:768c97e1cd505420a3feb6e9512bfe32d88049ff1f0c1fa2e3de20066e1b61f9
-size 5925
+oid sha256:42159e7eb93b39b53c39ef6014dbe31a0d3e87d66c8db7f34ca261889413da5d
+size 6501
