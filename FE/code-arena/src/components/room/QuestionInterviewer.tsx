9b7c81df35b7afdb7dfd93dc5052bf227fc9e4c1
허동민 <happycodingmin@gmail.com>
2024-08-08 14:41:48 +0900

    Update QuestionInterviewer.tsx
    Former-commit-id: 5a6140b7a7ba8cafa3fe117ee4117782287a907e

--- FE/code-arena/src/components/room/QuestionInterviewer.tsx
+++ FE/code-arena/src/components/room/QuestionInterviewer.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3983fffad3626a4c6f2c6cfd34b5e7596ca046ced7d218cd06fa31e61a93c469
-size 5951
+oid sha256:e3eefcc79b3e5ab333d77bfc4c540e4ee49156622e540da4d595ec26310fa9f5
+size 5862
