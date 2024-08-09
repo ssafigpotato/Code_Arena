d924d519d422dda410f858e34a1b60aef4c8d75c
ssafy11thseoul <ssaf010203@gmail.com>
2024-08-09 15:24:14 +0900

    S11P12A807-346 feat: 채팅 input, 메시지 컴포넌트 구현
    Former-commit-id: c1b9cf2988ce8c63fa76d75ce40bc6fdd6a5a0fd

--- FE/code-arena/src/components/room/chat-component/ChatForm.tsx
+++ FE/code-arena/src/components/room/chat-component/ChatForm.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:650dc73d76bdd85a65de77f3ed516428ac15f0d4d27c89e22c31d22005594da8
-size 425
+oid sha256:c1c829dc092f3ab10dde7b799f0ee5059a569c3f845b1b7360ab4ae0aeb9563d
+size 2971
