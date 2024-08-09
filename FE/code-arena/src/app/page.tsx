67cd137b8a1bd0cd513bbb987729672230be3d29
ryucream612 <ryucream612@naver.com>
2024-08-09 13:12:39 +0900

    S11P12A807-349 feat: 대쉬보드에 이미지 추가
    Former-commit-id: 588b7e52b399cecbf83196939b1af7f2f10795d6

--- FE/code-arena/src/app/page.tsx
+++ FE/code-arena/src/app/page.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2a3a1671312e72ac77a31647efc919a6bc385d45b93bbc8fd721b8de6dc5e5f4
-size 5436
+oid sha256:5f99469c7833e88af5985e15b665dd4cd36d82effb1febe47d67c22b1402fdfc
+size 5545
