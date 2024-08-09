c26706436b04ca9b85579eed0c287d6054e6112a
ryucream612 <ryucream612@naver.com>
2024-08-09 16:50:44 +0900

    S11P12A807-352 feat: 글 작성, 수정 시 마크다운 적용 가능한 에디터 적용
    Former-commit-id: 7a58b1633655f74f4fb336dc673205fdcfdd9281

--- FE/code-arena/src/app/community/[category]/edit/[boardId]/page.tsx
+++ FE/code-arena/src/app/community/[category]/edit/[boardId]/page.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d06fc02fb91bbb067970f3207ed578c490605b5e7e4cc54b7718a8b298b24c26
-size 6056
+oid sha256:56c29b33087a4a7d598f44aec1ea40a4aa1ccc9077db78f226e66b714fd7e76d
+size 7049
--- FE/code-arena/src/components/board/write/WriteContainer.tsx
+++ FE/code-arena/src/components/board/write/WriteContainer.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3ad313621968cd76bfe410e0282daddb534b8f8416fbf692a2baf646f5fc8506
-size 4784
+oid sha256:1a27516abb843c7eb9047681c8f41bb1a18a912414705be6caebc6d084bbd35d
+size 5713
