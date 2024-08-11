1ab9ab23330e04609afe438c26586a8e28e9199b
ssafy11thseoul <ssaf010203@gmail.com>
2024-08-11 14:44:50 +0900

    S11P12A807-343 fix: 코드 작성시 클래스명 기입 오류로 인한 NPE 방지
    Former-commit-id: 7b308bd17aecf41061e76ccc21b3499c3d616603

--- FE/code-arena/src/components/room/code-component/CodeTerminal.tsx
+++ FE/code-arena/src/components/room/code-component/CodeTerminal.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:44064b4bd2333649210ba9382562a89bac392378de6c47f185ecda06143f4917
-size 5602
+oid sha256:a19db7ae3456623b71a1b2d3e3b0aaa4004e870f1f97eeb285fd5f4a232385a3
+size 5606
