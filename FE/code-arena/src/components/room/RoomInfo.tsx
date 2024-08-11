f96098f0cb6a9a919aa2481f65acdd3de4efaaa8
Dohyungh <bley1217@gmail.com>
2024-08-11 17:21:17 +0900

    S11P12A807-355 fix: 4인 화상 송출 완료
    Former-commit-id: 15b3af4446cd7c45c38316f34dfd962311985341

--- FE/code-arena/src/components/room/RoomInfo.tsx
+++ FE/code-arena/src/components/room/RoomInfo.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e2cc58a120bc40157d73e01a63a39652d8298207d2da2ad356b11e38560d2a08
-size 8333
+oid sha256:ebe1093e2b623bb2366c93cc829a9ec734cc35417da432055fd64adbae560da3
+size 8237
