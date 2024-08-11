bc2de31fcafca1e1883870541bf975ad80796f0e
Dohyungh <bley1217@gmail.com>
2024-08-11 14:00:38 +0900

    S11P12A807-355 fix: 문제 생성, 문제 저장, 다른 면접관 화면에 띄우기
    Former-commit-id: b52e0dda0fafe76e0645be5ecb1c505499f20b32

--- FE/code-arena/src/components/room/GroupCall.tsx
+++ FE/code-arena/src/components/room/GroupCall.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ad03873f89e62c8fd79251dca0876e28716a9e0a43a4d0fc27188ab1d7ce67c6
-size 9722
+oid sha256:2c045a7162c7ee2b13e681047876828f7e61f6661cbbd7386768da687d88f3dd
+size 9707
--- FE/code-arena/src/store/aistore.tsx
+++ FE/code-arena/src/store/aistore.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0dd9ebcc53081184068923af95eae34f72d08850f95ddbd1df5243285704ef2d
-size 6395
+oid sha256:73ec6257153845fb89c518611af350b37018fe469762aba0f16a21d87ee2524d
+size 6359
