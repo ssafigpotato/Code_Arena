471086afb1df24fdb8d06737a60a219e089c8e61
inception010203 <inception010203@gmail.com>
2024-08-11 17:55:06 +0900

    S11P12A807-356 feat: 실행하기 버튼 클릭시 axios호출하고 example 받아오도록 작업
    Former-commit-id: 82221e1e75c8c503b4a5506195e701b0c96d8840

--- FE/code-arena/src/components/room/QuestionInterviewer.tsx
+++ FE/code-arena/src/components/room/QuestionInterviewer.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6dff2dde0ff6c7d886009eb5bb35750eb43fcfcb1f5650f8edb08fe52f6010a8
-size 9233
+oid sha256:7b85a15f8f9621b643aa4128f8b16208c0100a585e8f83b3974ea48a87756f89
+size 9314
