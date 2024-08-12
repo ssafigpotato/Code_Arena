f08906e916cb211f169dd74dd58f2d78fd0608c5
Dohyungh <bley1217@gmail.com>
2024-08-12 15:38:33 +0900

    S11P12A807-363 fix: 면접관 간에 문제생성 공유 완료
    Former-commit-id: f34aaa255d50a6df2ab3a62337c533916c30f600

--- FE/code-arena/src/components/room/roominfo-component/Timer.tsx
+++ FE/code-arena/src/components/room/roominfo-component/Timer.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a51b9fdee8d357f8332274a79208078d9e945d4b165d68ae2deba98ddd22405f
-size 2965
+oid sha256:fb38fa45956c0577adb733097d9d185b5b23ee2338cba721dd303f527b702957
+size 2971
