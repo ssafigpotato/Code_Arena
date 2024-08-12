054f188599ea536f703caba49537007220c6afc7
허동민 <gjehdals456@gmail.com>
2024-08-12 15:11:46 +0900

    S11P12A807-364 feat : 한 명의 구독자에게만 메세지가 가던 현상 해결 - done : 모든 구독자에게 보내도록 수정 - need : fanout exchange로 마이그레이션하면 좋을 것.
    Former-commit-id: 0511791ab067f18b095e03404535fdf9d9cdca14

--- BE/arena/src/main/java/com/example/arena/domain/code/service/RabbitMQService.java
+++ BE/arena/src/main/java/com/example/arena/domain/code/service/RabbitMQService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:02c57a6efdc4f100e5b5408ff356cf60415de1e13c1cef4b77852bf5941a466f
-size 2429
+oid sha256:6248bc896a7f68e6b2e7122d33094734803aa7d1475a3d67050ad36ca3920b9d
+size 2638
