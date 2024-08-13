51d779baa270e51bd395933122ffbf85699c58ab
허동민 <happycodingmin@gmail.com>
2024-08-13 17:36:37 +0900

    Update BE/arena/src/main/java/com/example/arena/domain/report/repository/ReportRepository.java
    Former-commit-id: 59ac8768958d3e5e2b3914aed383691d947e187c

--- BE/arena/src/main/java/com/example/arena/domain/report/repository/ReportRepository.java
+++ BE/arena/src/main/java/com/example/arena/domain/report/repository/ReportRepository.java
@@ -6,7 +6,9 @@ import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.arena.domain.report.entity.Report;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ReportRepository extends JpaRepository<Report, UUID> {
 
 	List<Report> findByTesterId(UUID memberId);
