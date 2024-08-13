f9ac075d86c701b071a6347e6ac19852f4106878
inception010203 <inception010203@gmail.com>
2024-08-13 17:20:26 +0900

    S11P12A807-391 fix: 터미널 실행,제출 전 기본 안내 메시지 색상 수정
    Former-commit-id: c4814d060358d2384c6080e4121153a67a1a23f0

--- FE/code-arena/src/components/room/code-component/CodeTerminal.tsx
+++ FE/code-arena/src/components/room/code-component/CodeTerminal.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c8d1039bc4c4f79d21c0112d1efd2138973ef43e7ef0c35e3109853f7a70c79e
-size 7898
+oid sha256:7ed2ea451fdd52d629d8285289d941fa49d2112635731de896b43eb2e983f925
+size 7997
