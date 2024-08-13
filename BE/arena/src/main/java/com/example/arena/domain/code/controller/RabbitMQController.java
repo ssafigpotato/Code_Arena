4fcc8bf5bfc3b6cc710df5f372ebbce7f023d45a
허동민 <gjehdals456@gmail.com>
2024-08-13 10:18:15 +0900

    S11P12A807-364 fix : generic type 사용하여 실행하기 제출하기 획일화 - done : generic type 사용 -> test 성공 시 exampleterminaldto 삭제 - need : test
    Former-commit-id: ee084578a4cf2776d04cbbe8905b7a498bf7a038

--- BE/arena/src/main/java/com/example/arena/domain/code/controller/RabbitMQController.java
+++ BE/arena/src/main/java/com/example/arena/domain/code/controller/RabbitMQController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:df3355338f7626a63e0f483c4e32eeafb0193151cb2ad8b4dcc38cdc86e04496
-size 2877
+oid sha256:c7d35faf4b4a68e66c0a1d46a562c37922e803bf938119b6929c80d400524a16
+size 2758
