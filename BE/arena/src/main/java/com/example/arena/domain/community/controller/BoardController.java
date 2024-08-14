ca74361a3230a0fa89aa25856d87138377c1c754
허동민 <gjehdals456@gmail.com>
2024-08-14 09:28:06 +0900

    feat : add /api/v1 request mapping
    Former-commit-id: bdaa7c978c6456737f89f5f373166275365e70a7

--- BE/arena/src/main/java/com/example/arena/domain/community/controller/BoardController.java
+++ BE/arena/src/main/java/com/example/arena/domain/community/controller/BoardController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:320afb81e6617939f955b7b3dbcfcecdcbacb677c5319922cb3e4ab3bdd5f6f5
-size 4524
+oid sha256:291c3db4ea2c5c254629e0a15c9ef88debc8a3987f9c60349ec916f2492037c0
+size 4531
--- BE/arena/src/main/java/com/example/arena/domain/report/controller/ReportController.java
+++ BE/arena/src/main/java/com/example/arena/domain/report/controller/ReportController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:aa988d36df0b6ccfb4d8672f7266367a40e2ec5b3c20c9483c9536946ceb6667
-size 1603
+oid sha256:d8419bd13530d939c422a5ef531e34bfeef79577a5fa58b06fd61934cb9793ce
+size 1797
