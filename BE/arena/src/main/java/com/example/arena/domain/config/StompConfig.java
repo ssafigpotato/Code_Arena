3519a705efa6a0433b524ade27becdaf536642d5
허동민 <gjehdals456@gmail.com>
2024-08-14 17:55:25 +0900

    feat : url config change
    Former-commit-id: 1f74a2ff5c3e4afb230783e712edcadbe421f35f

--- BE/arena/src/main/java/com/example/arena/domain/config/StompConfig.java
+++ BE/arena/src/main/java/com/example/arena/domain/config/StompConfig.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:62dfbcdabbfad279c71c1333f5febf661f569a336a005da3955d67e112c3da30
-size 1591
+oid sha256:71a13b8b5c8e5a0d07be4159de44f055f52e5f06a88a66b7015e501254f49f21
+size 1598
