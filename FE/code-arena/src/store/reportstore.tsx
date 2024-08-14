111a04e99eb2957fbff4e2b8de46468dc2c8277a
ryucream612 <ryucream612@naver.com>
2024-08-14 15:01:52 +0900

    S11P12A807-387 feat: 레포트 상세 연동 - need: 백과의 연동 테스트 필요 - done: api.ts에서 경로 변경
    Former-commit-id: 7bae716ae1b822a35b2321eafd4c915b6bf84ddd

--- FE/code-arena/src/store/reportstore.tsx
+++ FE/code-arena/src/store/reportstore.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4872046d21378cf6e13d57f8e5cb46b5dce00259fb767f8aca04db824157b9fc
-size 3011
+oid sha256:bf695a881f567c0ff0d922c9a45cb8d6486d107668b36ca6e5a4d6397202a97c
+size 3564
