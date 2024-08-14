8a2dab58d242dbe4dcc9920f59439ed6348a1612
ryucream612 <ryucream612@naver.com>
2024-08-14 10:50:49 +0900

    S11P12A807-386 feat: 레포트 목록 연동 코드 작성 - need: 백엔드와의 연동 확인 필요
    Former-commit-id: ad390b6fe08158c3c4b5520bf830059e724d8273

--- FE/code-arena/src/app/mypage/page.tsx
+++ FE/code-arena/src/app/mypage/page.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:84d217ab359df148e8a54979230cab8a53fbcc2ef9ea7d83fd18d70aa607eb49
-size 1359
+oid sha256:f5e618ea403942b61bb452bdd4dc16aa72eae911e63e031cb0944a2314f18f2e
+size 1362
