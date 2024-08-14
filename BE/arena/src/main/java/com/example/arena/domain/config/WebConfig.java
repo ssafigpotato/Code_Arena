e38ffaf6fe0d336be99a027a5b8fd0ea659c01be
허동민 <gjehdals456@gmail.com>
2024-08-14 17:55:25 +0900

    feat : url config change
    Former-commit-id: fc3f7137bb224ff5e7f7831cf6bbb57ce37fa71c

--- BE/arena/src/main/java/com/example/arena/domain/config/WebConfig.java
+++ BE/arena/src/main/java/com/example/arena/domain/config/WebConfig.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:89c5f04eebd7ecefdca8c98b1c94ac4c4d647d77e118d1a14158d61cd8e2ac70
-size 992
+oid sha256:a13a8bafa292164452b7d6187a61b3113c228cf0d1e590e07867725f7bcc97f8
+size 1016
--- FE/code-arena/src/utils/api.ts
+++ FE/code-arena/src/utils/api.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0673c2546ff58ca3d7ecc086646d2747079f88aeac8a2e27426c6a0661e10693
+oid sha256:8aa0f627f01c8c21d442e32c36adbd954c1397b93ee0af72f75bca61519e1d24
 size 1412
