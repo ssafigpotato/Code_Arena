1450edf67460832ba7dd7987ea911bf399e77752
ryucream612 <ryucream612@naver.com>
2024-08-14 15:01:52 +0900

    S11P12A807-387 feat: 레포트 상세 연동 - need: 백과의 연동 테스트 필요 - done: api.ts에서 경로 변경
    Former-commit-id: 9eb94ea672f7123ae6ff6270190eb19fdc9dd69d

--- FE/code-arena/src/components/report/ReviewCard.tsx
+++ FE/code-arena/src/components/report/ReviewCard.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3c9f808154bf63647c8887bc03ed80f2be177732941bcd223b9ff56924c09d7f
-size 2767
+oid sha256:72ef97dec86e52458aaeda616a4244e50f0216cc8d733e106294be707e409cdc
+size 2734
--- FE/code-arena/src/components/room/RoomInfo.tsx
+++ FE/code-arena/src/components/room/RoomInfo.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:14802b64231de403f2133bb5b1be26d6d79efa6137a0c20226ef43a6ccfeeb68
-size 8687
+oid sha256:3e562c2e3ca8032f9f347eb9729d5b39b13b0e7cbf46828eb26b0306cf3ca3b6
+size 9160
