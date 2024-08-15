7e6942c277adc2145db6bcb89056b0fe2e86f643
Dohyungh <bley1217@gmail.com>
2024-08-15 12:03:30 +0900

    S11P12A807-404 fix: 응시자/면접관 표시 수정
    Former-commit-id: f453abb0c86e679b9e7f8a68a7a105ac5b7934be

--- FE/code-arena/src/store/roomstore.tsx
+++ FE/code-arena/src/store/roomstore.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5ec98df15dab322c9edbc7b0cf5aef9e79df1ebd996c3cbf60081da84db9b9ba
-size 4818
+oid sha256:7a66ce9888cec31cabee32c925444814bf10e8b26494d980aeda7330896482dc
+size 4821
